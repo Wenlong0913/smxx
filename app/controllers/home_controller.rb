140a34939fff674774198762e6956efbeb5934ac
cheng <cheng@tanmer.com>
2016-11-10 17:42:50 +0800

    add_card

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,16 @@
 class HomeController < ApplicationController
   acts_as_themeable :choose_theme
+
   def index
   end
 
 private
-
   def choose_theme
     case params[:theme]
     when 'card' then 'card'
     when 'admin' then 'color_admin'
-    else 'card'
+    else
+      'color_admin'
     end
   end
 end
