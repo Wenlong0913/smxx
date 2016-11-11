da03f60a43952fa3478bdd0553d00349e4e9f5de
xiaohui <xiaohui@zhangxh.net>
2016-11-11 17:25:35 +0800

    fix site spec bugs

--- spec/models/site/destroy_spec.rb
+++ spec/models/site/destroy_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Site::Destroy', type: :model do
+  let(:user) { create(:user) }
   it do
-    site = create(:site)
-    flag, _ = Site::Destroy.(site)
-    expect(flag).to be(true)
-    expect(Site.exists?(site.id)).to be(false)
+    site = create(:site, user: user)
+    Site::Destroy.(site)
+    expect(Site.exists?(site.id)).to eq(false)
   end
 end
--- spec/models/site/update_spec.rb
+++ spec/models/site/update_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Site::Update', type: :model do
+  let(:user) { create(:user) }
   it 'for title' do
-    theme = create(:theme)
-    site = create(:site, theme: theme)
+    site = create(:site, user: user)
     flag, new_site = Site::Update.(site, title: 'new title')
-    expect(flag).to be(true)
-    expect(new_site.title).to be(site.title)
+    expect(flag).to eq(true)
+    expect(new_site.title).to eq(site.title)
   end
 
   it 'for description' do
-    theme = create(:theme)
-    site = create(:site, theme: theme)
+    site = create(:site, user: user)
     flag, new_site = Site::Update.(site, description: 'new description')
-    expect(flag).to be(true)
-    expect(new_site.description).to be(site.description)
+    expect(flag).to eq(true)
+    expect(new_site.description).to eq(site.description)
   end
 
   it 'with existing record' do
-    site = create(:site)
-    site2 = create(:site2)
-    flag, _ = Site::Update.(site, site2.attributes.slice(:title, :description))
-    expect(flag).to be(false)
+    site = create(:site, user: user)
+    site2 = create(:site2, user: user)
+    flag, _ = Site::Update.(site, site2.attributes.slice('title', 'description'))
+    expect(flag).to eq(false)
   end
 end
