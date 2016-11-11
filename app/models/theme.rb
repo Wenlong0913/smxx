b14de6c684908e07fc5242de379a8e9a72530f6e
yegang <yegang.avvo@gmail.com>
2016-11-11 13:37:02 +0800

    完善theme model

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,2 +1,7 @@
 class Theme < ApplicationRecord
+  has_many :theme_configs
+  has_many :sites, through: :theme_configs
+
+  validates_presence_of :name, :display_name
+  validates_uniqueness_of :name, :display_name
 end
--- spec/models/theme/update_spec.rb
+++ spec/models/theme/update_spec.rb
@@ -5,26 +5,26 @@ RSpec.describe 'Theme::Update', type: :model do
   let(:theme) { create(:theme) }
 
   it 'for name' do
-    flag, theme = Theme::Update.(theme, name: 'new name')
+    flag, updated_theme = Theme::Update.(theme, name: 'new name')
     expect(flag).to be(true)
-    expect(theme.name).to be('new name')
+    expect(updated_theme.name).to eq('new name')
   end
 
   it 'with existing name' do
     theme2 = create(:theme2)
-    flag, theme = Theme::Update.(theme, name: theme2.name)
+    flag, updated_theme = Theme::Update.(theme, name: theme2.name)
     expect(flag).to be(false)
   end
 
   it 'for display_name' do
-    flag, theme = Theme::Update.(theme, display_name: 'new display name')
+    flag, updated_theme = Theme::Update.(theme, display_name: 'new display name')
     expect(flag).to be(true)
-    expect(theme.display_name).to be('new display name')
+    expect(updated_theme.display_name).to eq('new display name')
   end
 
   it 'with existing display_name' do
     theme2 = create(:theme2)
-    flag, theme = Theme::Update.(theme, display_name: theme2.display_name)
+    flag, updated_theme = Theme::Update.(theme, display_name: theme2.display_name)
     expect(flag).to be(false)
   end
 
