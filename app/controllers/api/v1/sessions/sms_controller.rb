9e81fdf6ded595ddc23e4137cf3ed0a77267d39e
linan <linan.avvo@gmail.com>
2016-11-15 15:36:05 +0800

    手机登录

--- app/controllers/api/v1/sessions/sms_controller.rb
+++ app/controllers/api/v1/sessions/sms_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::Sessions::SmsController < Api::V1::BaseController
+
+  def create
+    t = Sms::Token.new(params[:mobile_phone])
+    code = (10000 + SecureRandom.random_number(10**8)).to_s[-5..-1]
+    body = Settings.mobile.auth_token_template.gsub('#code#', code)
+    t.create code: code, message: body
+    unless Rails.env.development?
+      response = t.post!
+      response.valid!
+    end
+    render json: {message: '验证码发送成功！'}
+  end
+
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Users::SessionsController < Devise::SessionsController
+  def create
+    login_type = params[:login_type]
+    if login_type == "mobile"
+      login_with_mobile
+    end
+  end
+
+  private
+    def login_with_mobile
+      mobile_user = MobileUser.find_by(phone_number: params[:mobile_phone])
+      if mobile_user
+        t = Sms::Token.new(params[:mobile_phone])
+        if (Rails.env.development? && params[:token] == '1234') || t.valid?(params[:token])
+          sign_in mobile_user.user
+          render json: {}
+        else
+          render json: {error: '验证码错误！'}
+        end
+      else
+        render json: {error: '您尚未注册！'}
+      end
+    end
 end
