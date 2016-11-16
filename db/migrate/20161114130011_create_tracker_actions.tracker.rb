4f38ed5cc29a3b9c4a3f32c90d924e79e0fe8f66
cheng <cheng@tanmer.com>
2016-11-16 11:43:06 +0800

    edit

--- db/migrate/20161114130011_create_tracker_actions.tracker.rb
+++ db/migrate/20161114130011_create_tracker_actions.tracker.rb
@@ -3,7 +3,7 @@ class CreateTrackerActions < ActiveRecord::Migration[5.0]
   def change
     create_table :tracker_actions do |t|
       t.string :name
-      t.string :controller_name
+      t.string :controller_path
       t.string :action_name
 
       t.timestamps
