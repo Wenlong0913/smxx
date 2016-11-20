e116b9b74656d43255cf5c29861f8892a9a21cba
cheng <cheng@tanmer.com>
2016-11-20 23:18:49 +0800

    PageStatisticsController

--- app/controllers/admin/page_statistics_controller.rb
+++ app/controllers/admin/page_statistics_controller.rb
@@ -1,5 +1,24 @@
 class Admin::PageStatisticsController< Admin::BaseController
   def index
-    @visits = Tracker::Visit.all.order(created_at: :desc).page(params[:page]).per(50)
+
+    if params[:times]
+      # Tracker::Visit.where("created_at between ? and ?", today.beginning_of_day, today.end_of_day)
+    else
+      today = Date.today
+      visits = Tracker::Visit.where("created_at between ? and ?", today.beginning_of_day, today.end_of_day)
+    end
+
+
+    uniq_visits_url = Tracker::Visit.all.map(&:url).uniq
+    @visits_data = []
+    uniq_visits_url.each do |url|
+      this_page_data = visits.select { |visit| visit.url == url }
+      next unless this_page_data.present?
+      @visits_data.push({
+        url: url,
+        count: this_page_data.count,
+        ip: this_page_data.map(&:ip_address).uniq.size
+      })
+    end
   end
 end
