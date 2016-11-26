f0d2a0f0ceeb1a44f74f6b5d9bb4f50efd3031a1
xiaohui <xiaohui@zhangxh.net>
2016-11-27 00:09:18 +0800

    添加seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-%w(admin agent user).each do |name|
+%w(admin agent).each do |name|
   Role.find_or_create_by name: name
 end
 
-User::Create.(mobile_phone: '15328077520')
+_, admin = User::Create.(mobile_phone: '15328077520', nickname: '管理员', password: 'abcd1234')
+_, agent = User::Create.(mobile_phone: '15328077521', nickname: '商家', password: 'abcd1234')
+_, user = User::Create.(mobile_phone: '15328077522', nickname: '用户', password: 'abcd1234')
+
+admin.add_role :admin
+agent.add_role :agent
