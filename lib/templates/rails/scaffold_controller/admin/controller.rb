70b5d31e72f9dc5682eac81192b4a2cd6f4cea95
xiaohui <xiaohui@zhangxh.net>
2016-11-29 22:14:33 +0800

    更新scaffold view模版，修复了所有错误

--- lib/templates/rails/scaffold_controller/admin/controller.rb
+++ lib/templates/rails/scaffold_controller/admin/controller.rb
@@ -1,5 +1,5 @@
 <%
-def strip_module(s)
+def strip_admin_module(s)
   s.gsub(/^Admin::/, '')
 end
 # binding.pry
@@ -14,8 +14,8 @@ class <%= controller_class_name %>Controller < Admin::BaseController
 
   # GET <%= route_url %>
   def index
-    authorize <%= strip_module(class_name) %>
-    @<%= plural_table_name %> = <%= orm_class.all(strip_module(class_name)) %>
+    authorize <%= strip_admin_module(class_name) %>
+    @<%= plural_table_name %> = <%= orm_class.all(strip_admin_module(class_name)) %>
   end
 
   # GET <%= route_url %>/1
@@ -25,8 +25,8 @@ class <%= controller_class_name %>Controller < Admin::BaseController
 
   # GET <%= route_url %>/new
   def new
-    authorize <%= strip_module(class_name) %>
-    @<%= singular_table_name %> = <%= orm_class.build(strip_module(class_name)) %>
+    authorize <%= strip_admin_module(class_name) %>
+    @<%= singular_table_name %> = <%= orm_class.build(strip_admin_module(class_name)) %>
   end
 
   # GET <%= route_url %>/1/edit
@@ -36,8 +36,8 @@ class <%= controller_class_name %>Controller < Admin::BaseController
 
   # POST <%= route_url %>
   def create
-    authorize <%= strip_module(class_name) %>
-    @<%= singular_table_name %> = <%= orm_class.build(strip_module(class_name), "permitted_attributes(@#{singular_table_name})") %>
+    authorize <%= strip_admin_module(class_name) %>
+    @<%= singular_table_name %> = <%= orm_class.build(strip_admin_module(class_name), "permitted_attributes(#{strip_admin_module(class_name)})") %>
 
     if @<%= orm_instance.save %>
       redirect_to <%= singular_table_name %>_path(@<%= singular_table_name %>), notice: <%= "'#{human_name} 创建成功.'" %>
@@ -49,7 +49,7 @@ class <%= controller_class_name %>Controller < Admin::BaseController
   # PATCH/PUT <%= route_url %>/1
   def update
     authorize @<%= singular_table_name %>
-    if @<%= strip_module(orm_instance.update("permitted_attributes(@#{singular_table_name})")) %>
+    if @<%= strip_admin_module(orm_instance.update("permitted_attributes(@#{singular_table_name})")) %>
       redirect_to <%= singular_table_name %>_path(@<%= singular_table_name %>), notice: <%= "'#{human_name} 更新成功.'" %>
     else
       render :edit
@@ -59,14 +59,14 @@ class <%= controller_class_name %>Controller < Admin::BaseController
   # DELETE <%= route_url %>/1
   def destroy
     authorize @<%= singular_table_name %>
-    @<%= strip_module(orm_instance.destroy) %>
+    @<%= strip_admin_module(orm_instance.destroy) %>
     redirect_to <%= index_helper %>_url, notice: <%= "'#{human_name} 删除成功.'" %>
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_<%= singular_table_name %>
-      @<%= singular_table_name %> = <%= orm_class.find(strip_module(class_name), "params[:id]") %>
+      @<%= singular_table_name %> = <%= orm_class.find(strip_admin_module(class_name), "params[:id]") %>
     end
 
     # Only allow a trusted parameter "white list" through.
