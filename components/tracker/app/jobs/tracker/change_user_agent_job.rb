8832d1c665ddc4a16fbcae434dc445174f5c8f0b
cheng <cheng@tanmer.com>
2016-11-30 11:50:32 +0800

    edit

--- components/tracker/app/jobs/tracker/change_user_agent_job.rb
+++ components/tracker/app/jobs/tracker/change_user_agent_job.rb
@@ -5,9 +5,6 @@ module Tracker
     def perform(visit_id)
       # Do something later
       visit = Tracker::Visit.find(visit_id)
-      unless visit.user_agent['string'].present?
-        visit.update(user_agent: {'string': visit.user_agent})
-      end
       browser = Browser.new(visit.user_agent['string'])
       visit.user_agent['name'] = browser.name
       visit.user_agent['platform'] = browser.platform.name
