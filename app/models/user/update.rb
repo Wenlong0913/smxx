dbdf9954a349e47febae610dfa1f91255dbce243
linan <linan.avvo@gmail.com>
2016-12-01 14:28:11 +0800

    fix bugs

--- app/models/user/update.rb
+++ app/models/user/update.rb
@@ -4,22 +4,23 @@ class User
       flag = true
       user = user_or_id.is_a?(User) ? user_or_id : User.find(user_or_id)
       mobile_phone = attributes[:mobile_phone]
-      role_ids = attributes[:role_ids]
 
       if current_user
-        role_ids.each do |role_id|
-          next if role_id.blank?
-          # 没有人可以分配超级管理员的角色
-          role_ids.delete(role_id) if Role.find_by(id: role_id).try(:name) == 'super_admin'
-          # 只有超级管理员可以分配管理员的角色
-          role_name = Role.find(role_id).name
-          unless current_user.has_role?('super_admin')
-            role_ids.delete(role_id) if role_name == 'admin'
+        role_ids = attributes[:role_ids]
+        if role_ids
+          role_ids.each do |role_id|
+            next if role_id.blank?
+            # 没有人可以分配超级管理员的角色
+            role_ids.delete(role_id) if Role.find_by(id: role_id).try(:name) == 'super_admin'
+            # 只有超级管理员可以分配管理员的角色
+            role_name = Role.find(role_id).name
+            unless current_user.has_role?('super_admin')
+              role_ids.delete(role_id) if role_name == 'admin'
+            end
+            attributes[:role_ids] = role_ids
           end
         end
       end
-
-      attributes[:role_ids] = role_ids
 
       if mobile_phone
         user.mobile.phone_number = mobile_phone
