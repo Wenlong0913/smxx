fa3a4313796b8985074bb7bb0593d727d0fe6df4
xiaohui <xiaohui@zhangxh.net>
2016-12-07 21:21:09 +0800

    解决I18n的解析问题

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,5 +1,18 @@
 class ApplicationDecorator < SimpleDelegator
 
+  # 解决I18n的解析问题
+  def self.model_class
+    self.name.gsub(/Decorator$/, '').constantize
+  end
+
+  def self.model_name
+    model_class.model_name
+  end
+
+  def self.human_attribute_name(*args)
+    model_class.human_attribute_name(*args)
+  end
+
   def object
     __getobj__
   end
