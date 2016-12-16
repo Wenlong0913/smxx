8800c923e69f0aa0eec63cc5b54e836bd3285867
cheng <cheng@tanmer.com>
2016-12-16 19:39:49 +0800

    改变agent引用js/css

---
+++ app/assets/themes/color_admin/agent/application.js
@@ -0,0 +1,13 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require turbolinks-jquery
+//= require color_admin/plugins/jquery/jquery-migrate-1.1.0
+//= require color_admin/plugins/gritter/js/jquery.gritter
+//= require color_admin/plugins/jquery-ui/ui/jquery-ui
+//= require color_admin/plugins/bootstrap/js/bootstrap
+//= require color_admin/js/apps
+//= require agent
+$(document).ready(function() {
+  App.init();
+});
