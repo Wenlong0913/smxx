7b7739414db44b529ad2db3857aed3528df28f55
xiaohui <xiaohui@zhangxh.net>
2016-12-16 13:06:29 +0800

    use native javascript to get csrf-token

--- webpack/concerns/http.js
+++ webpack/concerns/http.js
@@ -1,13 +1,18 @@
 var Vue = require('vue')
 Vue.use(require('vue-resource'))
-
 // Vue.http.options.root = '/api';
-Vue.http.interceptors.push({
-  request: function (request) {
-    Vue.http.headers.common['X-CSRF-Token'] = $('[name="csrf-token"]').attr('content');
-    return request;
-  },
-  response: function (response) {
-    return response;
+Vue.http.interceptors.push(function(){
+  return {
+    request: function (request) {
+      var meta = document.querySelector('[name="csrf-token"]'),
+        content = meta && meta.getAttribute('content')
+      if(content){
+        Vue.http.headers.common['X-CSRF-Token'] = content;
+      }
+      return request;
+    },
+    response: function (response) {
+      return response;
+    }
   }
 });
