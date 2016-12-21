55afc646497a648bc4b32445b5670f82420744be
xiaohui <xiaohui@zhangxh.net>
2016-12-21 15:34:28 +0800

    配置部署文件

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,20 +16,23 @@ set :application, "tmf"
 set :repo_url, "git@gitlab.tanmer.com:tanmer/tmf.git"
 set :rvm_ruby_version, '2.3'
 
-before 'deploy:check', :check_project_and_branch do
-  ask :project_name
-  # Default branch is :master
-  ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-  quit("project_name 必须填写") if fetch(:project_name).to_s.strip.empty?
-  branch_prefix = "#{fetch(:project_name)}-"
-  quit("branch名称必须以#{branch_prefix}开头，否者不允许部署!") unless fetch(:branch).start_with?(branch_prefix)
-  set :deploy_to, "/data/www/#{fetch(:project_name)}"
-end
+before 'deploy:check', 'deploy:check:project_and_branch'
+after 'deploy:assets:precompile', 'webpack:default'
 
-after 'deploy:assets:precompile', :compile_webpack do
-  invoke 'webpack:install'
-  invoke 'webpack:precompile'
-end
+# before 'deploy:check', :check_project_and_branch do
+#   ask :project_name
+#   # Default branch is :master
+#   ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+#   quit("project_name 必须填写") if fetch(:project_name).to_s.strip.empty?
+#   branch_prefix = "#{fetch(:project_name)}-"
+#   quit("branch名称必须以#{branch_prefix}开头，否者不允许部署!") unless fetch(:branch).start_with?(branch_prefix)
+#   set :deploy_to, "/data/www/#{fetch(:project_name)}"
+# end
+#
+# after 'deploy:assets:precompile', :compile_webpack do
+#   invoke 'webpack:install'
+#   invoke 'webpack:precompile'
+# end
 
 # before 'deploy:check:directories', :check_project_and_branch
 
@@ -50,7 +53,7 @@ end
 append :linked_files, "config/database.yml", "config/secrets.yml", "config/settings.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/assets"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/assets", 'node_modules'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
--- lib/capistrano/tasks/webpack.rake
+++ lib/capistrano/tasks/webpack.rake
@@ -1,16 +1,25 @@
 namespace :webpack do
 
+  task :default => ['install', 'precompile'] do
+  end
+
   desc "Install npm packages for webpack"
-  task :install do
+  task :install => 'deploy:check:project_and_branch' do
     on roles(:assets) do |h|
-      execute "npm install"
+      within release_path do
+        execute :npm, :install
+      end
     end
   end
 
   desc "Compile assets for webpack"
-  task :precompile do
+  task :precompile => ['deploy:check:project_and_branch'] do
     on roles(:assets) do |h|
-      execute "rails webpack:compile"
+      within release_path do
+        with rails_env: fetch(:stage) do
+          execute :bundle, :exec, 'rails webpack:compile'
+        end
+      end
     end
   end
 end
