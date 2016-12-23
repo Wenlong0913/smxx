9c47616c450a52b99affad23320659b722bad689
linan <linan.avvo@gmail.com>
2016-12-23 13:58:23 +0800

    remove test

--- db/schema.rb
+++ db/schema.rb
@@ -195,14 +195,6 @@ ActiveRecord::Schema.define(version: 20161222080634) do
     t.index ["user_id"], name: "index_sites_on_user_id", using: :btree
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string   "name"
-    t.text     "desc"
-    t.integer  "gender"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "theme_configs", force: :cascade do |t|
     t.integer  "site_id"
     t.integer  "theme_id"
