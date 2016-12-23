fba20814d8a8528fb6bc18324ef9cf0abcd2681e
linan <linan.avvo@gmail.com>
2016-12-23 11:50:02 +0800

    优化代码

--- webpack/components/image_upload/index.vue
+++ webpack/components/image_upload/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-      <input type="file" :name="name"/>
-      <img :src="imageUrl" alt="">  
-    </div>   
+  <div>
+    <input type="file" :name="name"/>
+    <img :src="imageUrl" alt="">
+  </div>
 </template>
 
 <script>
   import Slim from './slim.commonjs'
   export default {
-    props: ['name', 'autoUpload', 'server', "imageUrl"],
+    props: ['name', 'autoUpload', 'server', 'imageUrl'],
     mounted (){
       var vm = this;
       var slim = new Slim(vm.$el, {
@@ -16,7 +16,7 @@
         push: vm.autoUpload,
         label: '选择图片',
         buttonConfirmLabel: '确定',
-        buttonCancelLabel: '删除'
+        buttonCancelLabel: '取消'
       });
     }    
   }
