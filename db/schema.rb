47a17acbcb07af95796d6a6bfce158cc426db813
xiaohui <xiaohui@zhangxh.net>
2016-12-26 15:33:20 +0800

    add type to catalogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221062959) do
+ActiveRecord::Schema.define(version: 20161226073251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20161221062959) do
     t.integer  "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "type"
     t.index ["parent_id"], name: "index_catalogs_on_parent_id", using: :btree
   end
 
