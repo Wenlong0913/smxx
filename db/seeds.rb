85cb8e6636d4d85f56d6085ae16efb9f832f9afb
xiaohui <xiaohui@zhangxh.net>
2016-12-26 22:35:16 +0800

    添加User::MAIN_ID为网站主用户，给默认的site.user_id赋值

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
   Role.find_or_create_by name: name
 end
 
-_, admin = User::Create.(mobile_phone: '15328077520', nickname: '管理员', password: 'abcd1234')
+_, admin = User::Create.(id: 1, mobile_phone: '15328077520', nickname: '管理员', password: 'abcd1234')
 _, agent = User::Create.(mobile_phone: '15328077521', nickname: '商家', password: 'abcd1234')
 _, user = User::Create.(mobile_phone: '15328077522', nickname: '用户', password: 'abcd1234')
 
