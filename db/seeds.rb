8f23a327f61dbedb3dd18b894ee315cdf6666c71
yegang <yegang@tanmer.com>
2016-12-30 14:46:31 +0800

    insert vendor and meterial catalog

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,16 +39,34 @@ content = ''
   cms_page = Cms::Page.create!(channel_id: cms_channel.reload.id, title: '这是新闻标题', description: '这里是页面的描述', content: content)
 end
 
-names = %w(日用品
-生鲜食品
-海外商品
-旅游
-运功
-娱乐
-电子
-电器
-)
+# names = %w(日用品
+# 生鲜食品
+# 海外商品
+# 旅游
+# 运功
+# 娱乐
+# 电子
+# 电器
+# )
 
-names.each do |name|
-  Catalog.find_or_create_by(name: name)
+# names.each do |name|
+#   Catalog.find_or_create_by(name: name)
+# end
+
+
+# 德格供应商
+puts "创建供应商"
+vendor_names = ["自购", "康鑫", "联利达五金", "友达", "锐丰", "壹佰", "盛世百龙", "义力", "山森科技", "精工", "申康", "同乐", "上海画宇", "大家木业", "蒙友", "朋诚心", "尚好佳", "同鑫", "君子兰", "银冠", "爱格", "恒泰"]
+vendor_names.each do |vendor_name|
+  _, vendor_user = Vendor::Create.(name: vendor_name, contact_name: vendor_name + '联系人', phone_number: '152133643' + (10..99).to_a.sample(1).join)
+end
+
+# 德格物料分类
+puts "创建物料分类"
+material_catalogs = {"柜体": %w(板材 五金 封边带 纸箱 气泡垫/珍珠棉 油漆 刀具 封口胶 胶 旋转鞋柜 密码抽 反转床 穿衣镜), "移门": %w(五金 皮纹/软包 玻璃/腰线 型材), "平开门": %w(五金 吸塑)}
+material_catalogs.each_pair do |material_catalog, sub_material_catalos|
+  _, material_catalog = MaterialCatalog::Create.(name: material_catalog)
+  sub_material_catalos.each do |sub_material|
+    _, sub_material_catalog = MaterialCatalog::Create.(name: sub_material, parent: material_catalog)
+  end
 end
