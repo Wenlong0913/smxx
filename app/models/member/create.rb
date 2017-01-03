5f44f0854cac2d59535ecd27347a8a81e892cb7d
cheng <cheng@tanmer.com>
2017-01-03 19:44:12 +0800

    edit

--- app/models/member/create.rb
+++ app/models/member/create.rb
@@ -1,27 +1,8 @@
 class Member
   Create =
     lambda do |attributes, user: nil|
-      flag = false
-      mobile_phone = attributes.delete(:mobile_phone)
+      attributes[:gender] = 'secret'if attributes[:gender]
       record = Member.new(attributes)
-      if mobile_phone
-        mobile = User::Mobile.where(phone_number: mobile_phone).first
-        if mobile
-          record.user = mobile.user
-          flag = record.save
-        else
-          attributes[:name] = "用户:" + mobile_phone unless attributes[:name]
-          user_flag, user = User::Create.(nickname: attributes[:name], mobile_phone: mobile_phone)
-          if user_flag
-            record.user = user
-            flag = record.save
-          else
-            record.errors.add(:phone_number, user.errors.messages[:mobile_phone])
-          end
-        end
-      else
-        record.errors.add(:phone_number, '手机号不能为空！')
-      end
-      [flag, record]
+      [record.save, record]
     end
 end
