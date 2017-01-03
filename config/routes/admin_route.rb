62cb5a96f718c302fcabef546fcf9f52d3a0798b
张小辉 <xiaohui@tanmer.com>
2017-01-03 12:14:03 +0800

    Update admin_route.rb

--- config/routes/admin_route.rb
+++ config/routes/admin_route.rb
@@ -26,7 +26,7 @@ module AdminRoute
 
         resources :users, :concerns => :paginatable
         namespace :user do
-          resources :weixins, :concerns => :paginatable
+          resources :weixins, only: [:index], :concerns => :paginatable
         end
 
         resources :products, :concerns => :paginatable
