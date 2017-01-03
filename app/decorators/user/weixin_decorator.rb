938e4acf4df066515169a21521759e071e9048e0
linan <linan.avvo@gmail.com>
2017-01-03 11:45:33 +0800

    change name

--- app/decorators/user/weixin_decorator.rb
+++ app/decorators/user/weixin_decorator.rb
@@ -1,14 +1,14 @@
 class User::WeixinDecorator < ApplicationDecorator
 
-  def display_headshot
-    headshot || 'logo.jpg'
+  def headshot
+    super || 'logo.jpg'
   end
 
-  def display_created_at
-    created_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
+  def created_at
+    super.localtime.strftime('%Y-%m-%d %H:%M:%S')
   end
 
-  def display_updated_at
-    updated_at.localtime.strftime('%Y-%m-%d %H:%M:%S')
+  def updated_at
+    super.localtime.strftime('%Y-%m-%d %H:%M:%S')
   end
 end
