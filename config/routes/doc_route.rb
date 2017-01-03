c7f8216b0e716154c665aa2d7b70402131b44088
linan <linan.avvo@gmail.com>
2017-01-03 10:01:01 +0800

    fix doc routes

--- config/routes/doc_route.rb
+++ config/routes/doc_route.rb
@@ -2,7 +2,7 @@ module DocRoute
   def self.extended(router)
     router.instance_exec do
       get :docs, to: 'docs#index', as: :doc_root
-      get "/docs/(:page)", to: 'docs#index', as: :doc, constraints: { page: /[^\/].+\.md/ }
+      get "/docs/(:page)", to: 'docs#index', as: :doc, constraints: { page: /[^\\]+\.md/ }
     end
   end
 end
