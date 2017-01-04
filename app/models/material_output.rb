9733b40a625f444482f969b9835faa906dc485ae
linan <linan.avvo@gmail.com>
2017-01-04 20:31:17 +0800

    优化

--- app/models/material_output.rb
+++ app/models/material_output.rb
@@ -1,6 +1,5 @@
 class MaterialOutput < ApplicationRecord
   audited
   has_many :material_output_details
-  attr_accessor :material_code, :material_number
   accepts_nested_attributes_for :material_output_details
 end
