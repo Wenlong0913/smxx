df25b74dab6ef31842bce9077751af7959f8a7c8
linan <linan.avvo@gmail.com>
2017-01-16 17:44:39 +0800

    schema

--- db/migrate/20170110022239_add_uuid_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddUuidToUsers < ActiveRecord::Migration[5.0]
-  self.execute <<-EOF
-    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  EOF
-  def change
-    add_column :users, :uuid, :uuid, default: 'uuid_generate_v4()'
-  end
-end
