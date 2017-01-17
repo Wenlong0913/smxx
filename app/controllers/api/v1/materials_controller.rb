d0e46c1f09b8d01361751295dd8f1c27d7600440
xiaohui <xiaohui@zhangxh.net>
2017-01-18 00:51:57 +0800

    规范化分页数据的属性名

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -1,12 +1,22 @@
+# 物料
+
 class Api::V1::MaterialsController < Api::V1::BaseController
   before_action :authenticate!
 
-  #
-  # params:
-  #   page: 页码
-  # => per: 每页显示数目
+  # 获得物料列表
+  # @param [Integer] page 页码
+  # @param [Integer] page_size 每页显示数据
+  # @return [JSON]
   def index
     authorize Material
-    render json: Material.all.page(params[:page]).per(params[:per])
+    page_size = params[:page_size] || 20
+    materials = Material.all.page(params[:page] || 1).per(page_size)
+    render json: {
+      materials: materials.as_json(only: %w(id name)),
+      page_size: page_size,
+      current_page: materials.current_page,
+      total_pages: materials.total_pages,
+      total_count: materials.total_count
+    }
   end
 end
