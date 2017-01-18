cd82689d863843473375df3e181aab27f11d031c
linan <linan.avvo@gmail.com>
2017-01-18 15:11:46 +0800

    create material

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -9,14 +9,25 @@ class Api::V1::MaterialsController < Api::V1::BaseController
   # @return [JSON]
   def index
     authorize Material
-    page_size = params[:page_size] || 20
+    page_size = params[:page_size].present? ? params[:page_size].to_i : 20
     materials = Material.all.page(params[:page] || 1).per(page_size)
     render json: {
-      materials: materials.as_json(only: %w(id name)),
+      materials: materials.as_json(only: %w(id name), methods: %w(stock)),
       page_size: page_size,
       current_page: materials.current_page,
       total_pages: materials.total_pages,
       total_count: materials.total_count
     }
   end
+
+  def create
+    authorize Material
+    flag, material = Material::Create.(permitted_attributes(Material))
+    if flag
+      render json: {status: 'ok'}
+    else
+      render json: {status: 'fail'}
+    end
+  end
+
 end
