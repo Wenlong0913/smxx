a80e0ba2008a9f1439eeec0cc6d82a94ca666eb7
张小辉 <xiaohui@tanmer.com>
2017-01-18 09:50:14 +0800

    Update material.rb

--- app/models/material.rb
+++ app/models/material.rb
@@ -4,7 +4,7 @@ class Material < Item
   validates_numericality_of :price, allow_blank: true
   has_many :image_item_relations, as: :relation
   has_many :image_items, :through => :image_item_relations
-  has_many :material_warehouse_items
+  has_many :material_warehouse_items, dependent: :destroy
   has_many :material_warehouses, through: :material_warehouse_items
 
   # 物料只属于本公司，不能设置为其他Site
