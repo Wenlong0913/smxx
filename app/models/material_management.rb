937076b7cf90411a4ff38ebe3f0d2f8607d6e7b6
yegang <yegang@tanmer.com>
2017-01-24 16:33:55 +0800

    modify material display

--- app/models/material_management.rb
+++ app/models/material_management.rb
@@ -4,4 +4,6 @@ class MaterialManagement < ApplicationRecord
   accepts_nested_attributes_for :material_management_details
   belongs_to :material_warehouse
   enum operate_type: [:output, :input]
+
+  before_save -> { self.operate_date = Date.today if self.operate_date.blank? }
 end
