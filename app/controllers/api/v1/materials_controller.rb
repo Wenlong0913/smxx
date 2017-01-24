493d515968e190a90cf7dbf75c9723dcc7d08d93
cheng <cheng@tanmer.com>
2017-01-24 18:27:28 +0800

    order edit

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -2,23 +2,28 @@
 
 class Api::V1::MaterialsController < Api::V1::BaseController
   before_action :authenticate!
-
+  before_action :set_order, only: [:index]
   # 获得物料列表
   # @param [Integer] page 页码
   # @param [Integer] page_size 每页显示数据
   # @return [JSON]
   def index
     authorize Material
-    page_size = params[:page_size].present? ? params[:page_size].to_i : 20
-    materials = params['search_content'].present? ? Material.where("name_py like :key OR name like :key", {key: ['%',params['search_content'].upcase, '%'].join}) : Material.all
-    materials = materials.order(created_at: :desc).page(params[:page] || 1).per(page_size)
-    render json: {
-      materials: material_json(materials),
-      page_size: page_size,
-      current_page: materials.current_page,
-      total_pages: materials.total_pages,
-      total_count: materials.total_count
-    }
+    if params[:order_id].present?
+      materials = @order.materials.order(created_at: :desc)
+      render json: {materials: materials}
+    else
+      page_size = params[:page_size].present? ? params[:page_size].to_i : 20
+      materials = params['search_content'].present? ? Material.where("name_py like :key OR name like :key", {key: ['%',params['search_content'].upcase, '%'].join}) : Material.all
+      materials = materials.order(created_at: :desc).page(params[:page] || 1).per(page_size)
+      render json: {
+        materials: material_json(materials),
+        page_size: page_size,
+        current_page: materials.current_page,
+        total_pages: materials.total_pages,
+        total_count: materials.total_count
+      }
+    end
   end
 
   def create
@@ -39,17 +44,22 @@ class Api::V1::MaterialsController < Api::V1::BaseController
       render json: {status: 'ok', material: material_json(material)}
     else
       render json: {status: 'failed', error_message:  material.errors.messages.inject(''){ |k, v| k += v.join(':') + '. '} }
-    end    
+    end
   end
 
   private
+
+  def set_order
+    @order = Order.where(id: params[:order_id]).first if params[:order_id].present?
+  end
+
   def set_material
     Material.find_by_id(params[:id])
   end
 
   def material_json(materials)
     materials.as_json(
-      only: %w(id name name_py catalog_id), 
+      only: %w(id name name_py catalog_id),
       methods: %w(stock image_item_ids),
       include: {
         catalog: { only: %w(id name), methods: %w(full_name) },
