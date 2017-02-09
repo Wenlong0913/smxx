ea06baf05c7f4f2f4cf0fa3e6db4f9fad70d72c2
linan <linan.avvo@gmail.com>
2017-02-09 18:31:18 +0800

    物料分类选择优化

--- app/controllers/api/v1/material_catalogs_controller.rb
+++ app/controllers/api/v1/material_catalogs_controller.rb
@@ -10,8 +10,9 @@ class Api::V1::MaterialCatalogsController < Api::V1::BaseController
     page_size = params[:page_size].present? ? params[:page_size].to_i : 20
     material_catalogs = params['name'].present? ? MaterialCatalog.where("name like :key", {key: ['%',params['name'].upcase, '%'].join}) : MaterialCatalog.all
     material_catalogs = material_catalogs.order(created_at: :desc).page(params[:page] || 1).per(page_size)
+    material_catalogs_data = material_catalogs.map{|mc| mc.self_and_descendants }.flatten.uniq
     render json: {
-      material_catalogs: material_catalog_json(material_catalogs),
+      material_catalogs: material_catalog_json(material_catalogs_data),
       page_size: page_size,
       current_page: material_catalogs.current_page,
       total_pages: material_catalogs.total_pages,
