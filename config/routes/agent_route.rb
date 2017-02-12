e19cf34b5b1c78a984a9bb2165c548c6ff9bab12
宋学江 <song@tanmer.com>
2017-02-12 11:16:47 +0800

    updage product

--- config/routes/agent_route.rb
+++ config/routes/agent_route.rb
@@ -4,11 +4,18 @@ module AgentRoute
       namespace :agent do
         get '/', to: 'home#index', as: :root
         get 'sign_in', to: 'sessions#new'
-        resources 'products'
+        resources 'products' do
+          member do
+            post 'process_shelves'
+          end
+        end
+        resources 'orders'
+        resources 'statistics'
+
         resources 'members' do
           resources 'member_notes'
         end
-        resources 'statistics'
+
         catalog_resources_for ProductCatalog, only: [:index]
         resources :market_pages, :concerns => :paginatable do
           member do
