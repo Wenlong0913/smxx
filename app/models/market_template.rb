9ed39f1f85a6ac946d5d42d5ad23bfe1fe9606d8
宋学江 <song@tanmer.com>
2017-02-13 21:55:15 +0800

    修改agent产品页布局

--- app/models/market_template.rb
+++ app/models/market_template.rb
@@ -1,5 +1,6 @@
 class MarketTemplate < ApplicationRecord
   audited
+  validates_presence_of :name, :base_path, :html_source
   belongs_to :catalog
   has_many :image_item_relations, as: :relation
   has_many :image_items, :through => :image_item_relations
