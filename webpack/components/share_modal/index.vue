437a4cf377e31bd410426f1cbf5a3dcaf5817a50
cheng <cheng@tanmer.com>
2017-02-17 23:09:27 +0800

    添加一个全屏图片显示组件

---
+++ webpack/components/share_modal/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <modal title="title" :show.sync="showModal" @cancel="showModal = false" okText='保存' cancelText="取消" @ok="showModal = false">
+    <div>
+      <image-album :server="server" @delete="delete_image" @selected="selected_images" :selected-list="imageListIds"></image-album>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '分享',
+      showModal: false,
+      url: 'lack'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
