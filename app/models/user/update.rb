00d0d55c48c9f50f9752a9b86f5966e4cd36af8c
linan <linan.avvo@gmail.com>
2017-02-21 15:10:13 +0800

    fix user update

--- app/models/user/update.rb
+++ app/models/user/update.rb
@@ -8,16 +8,20 @@ class User
       if current_user
         role_ids = attributes[:role_ids]
         if role_ids
-          role_ids.each do |role_id|
-            next if role_id.blank?
-            # 没有人可以分配超级管理员的角色
-            role_ids.delete(role_id) if Role.find_by(id: role_id).try(:name) == 'super_admin'
-            # 只有超级管理员可以分配管理员的角色
-            role_name = Role.find(role_id).name
-            unless current_user.has_role?('super_admin')
-              role_ids.delete(role_id) if role_name == 'admin' && current_user.id != user.id
+          if current_user.super_admin_or_admin?
+            role_ids.each do |role_id|
+              next if role_id.blank?
+              # 没有人可以分配超级管理员的角色
+              role_ids.delete(role_id) if Role.find_by(id: role_id).try(:name) == 'super_admin'
+              # 只有超级管理员可以分配管理员的角色
+              role_name = Role.find(role_id).name
+              unless current_user.has_role?('super_admin')
+                role_ids.delete(role_id) if role_name == 'admin' && current_user.id != user.id
+              end
+              attributes[:role_ids] = role_ids
             end
-            attributes[:role_ids] = role_ids
+          else
+            attributes.delete(:role_ids)
           end
         end
       end
