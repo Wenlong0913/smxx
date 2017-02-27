7c224bfa94c6907f152ba91a1d069d89eeddd2aa
linan <linan.avvo@gmail.com>
2017-02-27 17:40:24 +0800

    出库关联订单

--- app/models/material_management.rb
+++ app/models/material_management.rb
@@ -6,4 +6,22 @@ class MaterialManagement < ApplicationRecord
   enum operate_type: [:output, :input]
 
   before_save -> { self.operate_date = Date.today if self.operate_date.blank? }
+  after_create :update_order_material
+
+  def update_order_material
+    if order_code
+      order = Order.find_by(code: order_code)
+      material_management_details.each do |mmd|
+        order_material = order.order_materials.where(material_id: mmd.material_id).first
+        if order_material
+          if operate_type == 'input'
+            order_material.practical_number = order_material.practical_number.to_i - mmd.number
+          else
+            order_material.practical_number = order_material.practical_number.to_i + mmd.number
+          end
+          order_material.save!
+        end
+      end
+    end
+  end
 end
