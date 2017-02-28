dd6265af7d81dfeadb1315d3f69bc5e1c623045b
cheng <cheng@tanmer.com>
2017-02-28 10:47:51 +0800

    修改ｃａｔａｌｏｇ创建失败的问题

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -1,7 +1,7 @@
 class Catalog < ApplicationRecord
   audited
   has_closure_tree dependent: :destroy
+  store_accessor :features, :settings
 
   validates :name, uniqueness: {scope: :parent_id}, presence: true
-
 end
