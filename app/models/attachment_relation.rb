4443b664b9aaacaa9179e76f92f4b94b42126b0e
cheng <cheng@tanmer.com>
2017-02-28 16:51:37 +0800

    添加验证attachment_relation唯一性

--- app/models/attachment_relation.rb
+++ app/models/attachment_relation.rb
@@ -3,4 +3,5 @@ class AttachmentRelation < ApplicationRecord
 
   belongs_to :attachment
   belongs_to :relation, polymorphic: true
+  validates_uniqueness_of :attachment_id, scope: [:relation_type, :relation_id]
 end
