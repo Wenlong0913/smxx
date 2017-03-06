0326a115b421a5ebbe9a664ebc064336dbda6016
cheng <cheng@tanmer.com>
2017-03-06 22:43:13 +0800

    i-123

--- app/models/order/create.rb
+++ app/models/order/create.rb
@@ -1,6 +1,7 @@
 class Order
   Create =
     lambda do |attributes, user: nil|
+      attributes['price'] = attributes['price'].to_f * 100
       record = Order.new(attributes)
       [record.save, record]
     end
--- app/models/order/update.rb
+++ app/models/order/update.rb
@@ -1,6 +1,7 @@
 class Order
   Update =
     lambda do |record_or_id, attributes, user: nil|
+      attributes['price'] = attributes['price'].to_f * 100
       record = record_or_id.is_a?(Order) ? record_or_id : Order.find(record_or_id)
       record.assign_attributes attributes
       [record.save, record]
