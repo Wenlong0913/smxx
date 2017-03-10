ee9f81109a9e81b36c61dda6ecd8afe78658fdba
linan <linan.avvo@gmail.com>
2017-03-10 12:20:49 +0800

    product catalog

--- app/models/product_catalog/update.rb
+++ app/models/product_catalog/update.rb
@@ -2,6 +2,7 @@ class ProductCatalog
   Update =
     lambda do |record_or_id, attributes, user: nil|
       record = record_or_id.is_a?(Catalog) ? record_or_id : ProductCatalog.find(record_or_id)
+      attributes["settings"] = attributes["settings"].split(/[,，]/).map(&:strip) if attributes["settings"].present?
       record.assign_attributes attributes
       [record.save, record]
     end
