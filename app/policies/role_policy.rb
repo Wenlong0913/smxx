a019817f429de503c27971e3d27618009c043e11
yegang <yegang@tanmer.com>
2017-03-14 20:59:23 +0800

    optimize page

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -16,16 +16,4 @@ class RolePolicy < ApplicationPolicy
   def update_permission?
     edit_permission?
   end
-
-  def permitted_attributes_for_create
-    if user.super_admin_or_admin?
-      [:name, :permission_ids => []]
-    else
-      []
-    end
-  end
-
-  def permitted_attributes_for_update
-    permitted_attributes_for_create
-  end
 end
