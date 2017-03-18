aab75f19a11f25d323c03d4b11e0f7c61b8b79ce
宋学江 <song@tanmer.com>
2017-03-18 13:12:10 +0800

    updage migration

--- db/migrate/20170316092540_add_properties_to_cms_page.rb
+++ db/migrate/20170316092540_add_properties_to_cms_page.rb
@@ -2,6 +2,7 @@
 # http://rny.io/rails/postgresql/2013/07/28/tagging-in-rails-4-using-postgresql-arrays.html
 class AddPropertiesToCmsPage < ActiveRecord::Migration[5.0]
   def change
+    remove_column :cms_pages, :properties
     add_column :cms_pages, :properties, :string, array: true, default: []
     add_index  :cms_pages, :properties, using: 'gin'
   end
