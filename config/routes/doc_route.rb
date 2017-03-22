fb2bbb6e5e1be8134c8811182a87aef48dbb70dd
宋学江 <song@tanmer.com>
2017-03-22 15:57:45 +0800

    Add doc route

--- config/routes/doc_route.rb
+++ config/routes/doc_route.rb
@@ -1,8 +1,10 @@
 module DocRoute
   def self.extended(router)
     router.instance_exec do
-      get :docs, to: 'docs#index', as: :doc_root
-      get "/docs/(:page)", to: 'docs#index', as: :doc, constraints: { page: /.+\.md/ }
+      constraints(DocSubdomain) do
+        root :to => "docs#index"
+        get "/(:page)", to: 'docs#index', as: :doc, constraints: { page: /.+\.md/ }
+      end
     end
   end
 end
