7eb0c3c19a63d6c7cb972c0c6a444d2a2d890b27
linan <linan.avvo@gmail.com>
2017-03-22 16:20:28 +0800

    数量为空时不能出入库

--- app/models/material_management.rb
+++ app/models/material_management.rb
@@ -16,7 +16,7 @@
 class MaterialManagement < ApplicationRecord
   audited
   has_many :material_management_details, dependent: :destroy
-  accepts_nested_attributes_for :material_management_details
+  accepts_nested_attributes_for :material_management_details, reject_if: proc { |attributes| attributes['number'].blank? || attributes['number'].to_i == 0}
   belongs_to :material_warehouse
   enum operate_type: [:output, :input]
 
