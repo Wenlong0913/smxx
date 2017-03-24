bc5e1706924e7f882d12f21cb64a67d6e6a29f2e
宋学江 <song@tanmer.com>
2017-03-24 11:34:02 +0800

    add

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -6,6 +6,18 @@ import router from './router'
 import store from './store'
 import CordovaHelper from './utils/CordovaHelper'
 
+// Import F7
+import Framework7 from 'framework7'
+// Import F7 Vue Plugin
+import Framework7Vue from 'framework7-vue'
+
+// Import F7 iOS Theme Styles
+// import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
+// import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
+/* OR for Material Theme: */
+import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
+import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
+
 /* setup cordova */
 CordovaHelper.setup()
 /* fast click */
@@ -13,10 +25,32 @@ const FastClick = require('fastclick')
 FastClick.attach(document.body)
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   store,
+//   template: '<App/>',
+//   components: { App }
+// })
+
+// Init F7 Vue Plugin
+Vue.use(Framework7Vue)
+
+// Init App
 new Vue({
   el: '#app',
   router,
   store,
   template: '<App/>',
-  components: { App }
-})
+  // Init Framework7 by passing parameters here
+  framework7: {
+    root: '#app',
+    /* Uncomment to enable Material theme: */
+    // material: true,
+    routes: router
+  },
+  // Register App Component
+  components: {
+    app: { App }
+  }
+}).$mount('#app')
--- mobile/src/router/routes.js
+++ mobile/src/router/routes.js
@@ -5,5 +5,17 @@ import Login from 'views/Login'
 export default [
   { path: '/index.html', name: 'root', component: Home, alias: '/' },
   { path: '/login', name: 'login', component: Login },
-  { path: '*', component: NotFound }
+  { path: '*', component: NotFound },
+  {
+    path: '/about/',
+    component: require('./../views/about.vue')
+  },
+  {
+    path: '/form/',
+    component: require('./../views/form.vue')
+  },
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: require('./../views/dynamic-route.vue')
+  }
 ]
