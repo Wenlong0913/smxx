e74954726ab6bdb0fa5914b820e8dbafd2cbee75
cheng <cheng@tanmer.com>
2017-03-30 11:33:43 +0800

    添加site 的图片

--- app/models/site.rb
+++ app/models/site.rb
@@ -17,7 +17,8 @@ class Site < ApplicationRecord
   has_many :theme_configs
   has_one :active_theme_config, -> { where(active: true) }, class_name: 'ThemeConfig'
   has_many :items, dependent: :destroy
-  has_many :image_items, dependent: :destroy, as: :owner
+  has_many :image_item_relations, as: :relation
+  has_many :image_items, :through => :image_item_relations
   has_many :members, dependent: :destroy
   has_many :products, dependent: :destroy
   has_many :orders, dependent: :destroy
