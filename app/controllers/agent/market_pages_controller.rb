949d0ff4000202597743280b42fe560a6c80978d
cheng <cheng@tanmer.com>
2017-04-01 12:35:28 +0800

    修改链接为分享链接

--- app/controllers/agent/market_pages_controller.rb
+++ app/controllers/agent/market_pages_controller.rb
@@ -14,6 +14,8 @@ class Agent::MarketPagesController < Agent::BaseController
 
   def show
     authorize @market_page
+    resource = @market_page.sales_distribution_resources.first
+    @share_path = URI(request.scheme + "://" + request.host + ":" + request.port.to_s).merge(resource.share_path).to_s
     respond_to do |format|
       format.html
       format.json { render json: @market_page }
@@ -39,6 +41,12 @@ class Agent::MarketPagesController < Agent::BaseController
     respond_to do |format|
       format.html do
         if @market_page.save!
+          resource = SalesDistribution::Resource.find_or_create_by(
+            type_name: '营销活动',
+            user: current_user,
+            url: agent_frontend_market_page_url(@site, @market_page),
+            object: @market_page
+          )
           redirect_to agent_market_page_path(@market_page), notice: 'Market page 创建成功.'
         else
           render :new
--- app/models/market_page.rb
+++ app/models/market_page.rb
@@ -21,6 +21,7 @@ class MarketPage <  ApplicationRecord
   validates_presence_of :name, :site, :market_template
   has_many :image_item_relations, as: :relation
   has_many :image_items, :through => :image_item_relations
+  has_many :sales_distribution_resources, class_name: 'SalesDistribution::Resource', as: 'object'
 
   def value_for(title, *opt)
     self.features[title] if self.features
