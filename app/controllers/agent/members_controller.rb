3200acc61d6594fb78887686103d9bdbdb148bc7
cheng <cheng@tanmer.com>
2017-04-05 09:38:06 +0800

    agent 会员筛选查询

--- app/controllers/agent/members_controller.rb
+++ app/controllers/agent/members_controller.rb
@@ -6,11 +6,20 @@ class Agent::MembersController < Agent::BaseController
     authorize Member
     @agent_members = @site.members
     if params[:search].present?
-      # keyword = params[:search][:keywords]
       @filter_colums = %w(mobile_phone name)
       @agent_members = build_query_filter(Member.all, only: @filter_colums)
+      conditions = []
+      query = []
+      @agent_member_catalogs.each do |catalog|
+        if params[:search][catalog.key].present?
+          query << "(features ->> '"+catalog.key+"' like ?)"
+          conditions << params[:search][catalog.key]
+        end
+      end
+      conditions.unshift query.join(' and ')
+      @agent_members = @agent_members.where(conditions)
     end
-    @agent_members = @agent_members.page(params[:page]).per(1)
+    @agent_members = @agent_members.page(params[:page])
     respond_to do |format|
       format.html
       format.json { render json: @agent_members }
