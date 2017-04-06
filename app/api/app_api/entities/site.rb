185fff6d8d9dcb29542676b4debbd385bab034c4
hao <hao@tanmer.com>
2017-04-06 10:47:27 +0800

    update

--- app/api/app_api/entities/site.rb
+++ app/api/app_api/entities/site.rb
@@ -12,12 +12,16 @@ module AppAPI
       
       expose :address, documentation: { desc: '地址' }
       expose :image_items, using: AppAPI::Entities::ImageItem, as: :images
+      expose :properties, documentation: { desc: '标签备注' }
 
       expose :products, using: AppAPI::Entities::Product, if: ->(site, options) { (options[:includes] || []).include?(:products) }
       # # product detail
       # with_options if: ->(site, options) { options[:type] == :full_site } do |f|
       #   expose :products, using: AppAPI::Entities::Product
       # end
+      def properties
+        object.properties.map{|p| ::Site::PROPERTIES[p.to_sym]}.compact.join('，')
+      end
     end
   end
 end
