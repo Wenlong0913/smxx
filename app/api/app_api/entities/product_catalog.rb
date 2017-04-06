66e69e24b3efa5e3765cf984cb7cce9e98903061
宋学江 <song@tanmer.com>
2017-04-06 14:34:28 +0800

    updage master

--- app/api/app_api/entities/product_catalog.rb
+++ app/api/app_api/entities/product_catalog.rb
@@ -4,8 +4,6 @@ module AppAPI
       # 公开出来的属性
       expose_id
       expose :name, documentation: { desc: '分类名称' }
-
-
       expose :products, using: AppAPI::Entities::Product, if: ->(site, options) { (options[:includes] || []).include?(:products) }
     end
   end
