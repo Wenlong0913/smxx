dd738ce7a4cc512c5ae9e0503f5ff2cc5a89d82f
linan <linan.avvo@gmail.com>
2017-04-10 17:37:05 +0800

    add thumb image

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,7 +52,7 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-  validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
+  validates_attachment_file_name :avatar, matches: [/png\z/i, /jpe?g\z/i]
   validates_with AttachmentSizeValidator, attributes: :avatar, less_than: 10.megabytes
 
   attr_accessor :mobile_phone
