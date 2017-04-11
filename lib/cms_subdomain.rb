b7452caa6d4f02564df557cffe585532f77c9272
宋学江 <song@tanmer.com>
2017-04-11 21:46:17 +0800

    update side menu

--- lib/cms_subdomain.rb
+++ lib/cms_subdomain.rb
@@ -2,6 +2,9 @@ require 'api_subdomain'
 require 'root_domain'
 module CmsSubdomain
   def self.matches?(request)
-    request.subdomain.present? && !RootDomain.matches?(request) && !ApiSubdomain.matches?(request)
+    request.subdomain.present? &&
+    !RootDomain.matches?(request) &&
+    !DocSubdomain.matches?(request) &&
+    !ApiSubdomain.matches?(request)
   end
 end
