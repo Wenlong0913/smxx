0441d95b75915c427d40a30f808556a0604e018a
hao <hao@tanmer.com>
2017-04-11 16:04:45 +0800

    update template

--- mobile/src/pages/product_catalog_detail.vue
+++ mobile/src/pages/product_catalog_detail.vue
@@ -1,5 +1,6 @@
 <template>
 <div data-page="productdetail" class="page navbar-fixed page-on-center">
+  <!-- navbar -->
   <div class="navbar">
     <div class="navbar-inner">
       <div class="left sliding">
@@ -7,44 +8,89 @@
           <i class="icon icon-back"></i>
         </a>
       </div>
-      <div class="center">美客美</div>
+      <div class="center">{{ product_detail.name }}</div>
       <div class="right">
-        <a  @click="input" class="link icon-only">
+        <a @click="input" class="link icon-only">
           <i class="fa fa-cart-plus"></i>
         </a>
       </div>
     </div>
   </div>
+  
+  <!-- tabs -->
+  <div class="toolbar tabbar">
+    <div class="toolbar-inner">
+      <a href="#tab1" class="button active tab-link">项目介绍</a>
+      <a href="#tab2" class="button tab-link">用户评价</a>
+    </div>
+  </div>
+  <!-- page-content -->
   <div class="page-content hide-bars-on-scroll sidebar">
-    <div class="row">
-      <img :src="image1" class="col-100" alt="banner1">
+    <div class="tabs">
+      <div id="tab1" class=" tab active">
+        <div class="row">
+          <img :src="image1" class="col-100" alt="banner1">
+        </div>
+        <div class="prductlist content-block">
+          <!-- title -->
+          <div class="item-link item-content">
+            <div class="item-inner">
+              <div class="item-title-row">
+                <div class="author" style="font-size: 16px;">{{ product_detail.name }}</div>
+              </div>
+              <div class="time" style="margin-top: -25px; font-size: 16px;">
+                <time datetime="2015-08-30">
+                  <i class="fa fa-clock-o"></i> {{ product_detail.service_time }}
+                </time>
+              </div>
+              <div class="clear"></div>
+            </div>
+          </div>
+          <!-- 描述 -->
+          <p style="margin: 5px 0;">服务：{{ product_detail.description }}</p>
+          <!-- 价格 -->
+          <div class="row">
+            <p class="col-50 color-default" style="margin: 5px 0; font-size: 16px;"> 
+              原价: <span style=" text-decoration:line-through;">￥{{ product_detail.old_price }}</span>
+            </p>
+            <p class="col-50 color-amber  text-right" style="margin: 5px 0; font-weight: 700; font-size: 16px;">现价: ￥{{ product_detail.sell_price }}</p>
+          </div>
+           <!-- tags -->
+          <div class="row">
+            <div class="col-25">
+              <i class="fa fa-check-circle-o color-green"></i> {{ product_detail.properties }}
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="content-block">
+          <h3 class="col-100">项目介绍</h3>
+          <p class="item-text" v-html="product_detail.content"></p>
+        </div>
+        <hr>
+        <div class="content-block">
+          <h3 class="col-100">适用人群</h3>
+          <p class="item-text">{{ product_detail.responsive_person }}</p>
+        </div>
+        <hr>
+        <div class="content-block">
+          <h3 class="col-100">注意事项</h3>
+          <p class="item-text">{{ product_detail.warning_message }}</p>
+        </div>
+      </div>
+      <div id="tab2" class=" tab">
+        <div class="content-block">
+          <h2>评价</h2>  
+        </div>
+      </div>
     </div>
-    <div class="prductlist  content-block">
-      <h2 class="maintitle col-100">{{ product_detail.name }}</h2>
-      <h3 class="col-100 color-green">￥ {{ product_detail.sell_price }}</h3>
-      <div class="item-subtitle star color-amber ">★★★★☆</div>
-      <p class="">{{ product_detail.description }}</p>
-    </div>
-    <hr>
-    <div class="content-block">
-      <h3 class="col-100">项目介绍</h3>
-      <p class="item-text" v-html="product_detail.content"></p>
-    </div>
-    <hr>
-    <div class="content-block">
-      <h3 class="col-100">适用人群</h3>
-      <p class="item-text">{{ product_detail.responsive_person }}</p>
-    </div>
-    <hr>
-    <div class="content-block">
-      <h3 class="col-100">注意事项</h3>
-      <p class="item-text">{{ product_detail.warning_message }}</p>
-    </div>
-    <hr/>
-    <div class="buttonbar no-gutter">
-      <a class="cartfloating floating-button" @click="input">
-        <i class="fa fa-cart-plus"></i>
-      </a>
+  </div>
+
+  <!-- toolbar -->
+  <div class="toolbar toolbar-bottom">
+    <div class="toolbar-inner">
+      <div class="col-100" style="height: 48px; line-height: 48px; padding: 0 16px;">需支付 ￥{{ product_detail.sell_price }}</div>
+      <a href="#" class="link" @click="input">加入购物车</a>
     </div>
   </div>
 </div>
