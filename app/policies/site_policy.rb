7eff4fb55d9002337df281e7e1e546d8cd3bf60d
hao <hao@tanmer.com>
2017-04-12 10:53:18 +0800

    append score,sign_note

--- app/policies/site_policy.rb
+++ app/policies/site_policy.rb
@@ -36,7 +36,7 @@ class SitePolicy < ApplicationPolicy
   def permitted_attributes_for_create
     if user.super_admin_or_admin?
       [:user_id, :title, :site_hours, :content,
-        :contact_number, :contact_name, :is_sign, :comment,
+        :contact_number, :contact_name, :is_sign, :sign_note, :score, :comment,
         :lat, :lng, :address_line,
         :image_item_ids => [], :properties => []]
     else
