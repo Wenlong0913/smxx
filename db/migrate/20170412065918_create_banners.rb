8c5b1895e4f9cfa8365126346a89029454837024
yegang <yegang@tanmer.com>
2017-04-12 17:36:41 +0800

    完善banner

--- db/migrate/20170412065918_create_banners.rb
+++ db/migrate/20170412065918_create_banners.rb
@@ -2,7 +2,7 @@ class CreateBanners < ActiveRecord::Migration[5.0]
   def change
     create_table :banners do |t|
       t.string :title
-      t.string :banner_type
+      t.integer :banner_type
       t.string :image_url
       t.string :redirect_web_url
       t.string :redirect_app_url
