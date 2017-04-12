64a8d0fab1e4deee435a89d204dcfa94ae882b2b
hao <hao@tanmer.com>
2017-04-12 16:58:37 +0800

    udpates

--- mobile/src/pages/product_catalogs.vue
+++ mobile/src/pages/product_catalogs.vue
@@ -14,22 +14,44 @@
       </div>
     </div>
     <div class="page-content">
-      <div class="list-block media-list shop">
-        <ul>
-          <li  v-for="product in products">
-            <a :href="/product_catalog_detail/ + product.id" class="item-link item-content">
-              <div class="item-media">
-                <img src="./../assets/img/pic1.png" width="80" alt="">
+      <div class="content-block" style="margin: 10px 0;">
+        <div class="row" v-for="product in products">
+          <div class="card tablet-50">
+            <div class="banner productblock">
+              <img :src="product.images[0].image_url" class="col-100" :alt="product.name" > 
+                <a class="cartfloating floating-button" :href="/product_catalog_detail/ + product.id"><i class="fa fa-eye"></i>
+                </a>
+            </div>
+            <div class="content-block productdata">
+              <h2 class="maintitle col-100 margin_title_5">{{ product.name }}</h2>
+              <ul class="socials">
+                <li v-for="pro in product.properties" class="item_li">
+                  <i class="fa fa-tags color-green"></i>{{ pro }}
+                </li>
+              </ul>
+              <div class="row margin_top_5">
+                <div class="col-100 color_pink site_title">
+                  ￥{{ product.sell_price }}
+                </div>
+                <div class="col-100 service_color">
+                  <i class="fa fa-clock-o"></i> {{ product.service_time }}
+                </div>
               </div>
-              <div class="item-inner">
-                <div class="item-title" style="font-size: 16px;">{{ product.name }}</div>
-                <div class="item-text" style="text-align: center;">{{ product.description }}</div>
-                <div class="color-amber" style="text-align: center;">￥{{ product.sell_price }}</div>
-                <div class="item-text" style="text-align: center;">{{ product.service_time }} 月售{{ product.month_number }}</div>
+              <div class="row">
+                <div class="col-100">
+                  <p class="item-subtitle margin_start_5 item_star" v-if="product.score == 5">
+                    <span class="fa fa-star color_pink" v-for="s in product.score"></span>
+                    <span class="color_pink">顶级</span>
+                  </p>
+                  <p class="item-subtitle item_star margin_start_5" v-else>
+                    <span class="fa fa-star color_pink" v-for="s in product.score"></span>
+                  </p>
+                </div>
               </div>
-            </a>
-          </li>
-        </ul>
+            </div>
+            <div class="clear"></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
