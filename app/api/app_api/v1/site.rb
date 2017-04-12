48c3debaee953cbb16f7d41f4dcc7f0b95436c6c
linan <linan.avvo@gmail.com>
2017-04-12 10:20:25 +0800

    fix name

--- app/api/app_api/v1/site.rb
+++ app/api/app_api/v1/site.rb
@@ -22,7 +22,7 @@ module AppAPI::V1
         use :pagination
         # use :sort, fields: [:id, :created_at, :updated_at]
         optional :friends, type: Boolean, desc: "好友#{::Site.model_name.human}"
-        optional :favorite, type: String, values: ['private', 'friends', 'all'], desc: "私藏#{::Site.model_name.human}：我的私藏#{::Site.model_name.human}，好友私藏的#{::Site.model_name.human}，被私藏数高的#{::Site.model_name.human}"
+        optional :favorite, type: String, values: ['mine', 'friends', 'top3'], desc: "私藏#{::Site.model_name.human}：我的私藏#{::Site.model_name.human}，好友私藏的#{::Site.model_name.human}，被私藏数高的#{::Site.model_name.human}top3"
       end
       get do
         authenticate!
@@ -30,9 +30,9 @@ module AppAPI::V1
         if params[:favorite]
           sites =
             case params[:favorite]
-            when 'private' then sites.where(id: current_user.favorites.where(resource_type: 'Site').map(&:resource_id))
+            when 'mine' then sites.where(id: current_user.favorites.where(resource_type: 'Site').map(&:resource_id))
             when 'friends' then sites.where(id: User.joins(:favorites).where(resource_type: 'Site').where("users.id in ?", current_user.friends).pluck("resource_id"))
-            when 'all'     then sites.left_joins(:favorites).group("sites.id").order('COUNT(favorite_entries.id) DESC')
+            when 'top3'     then sites.left_joins(:favorites).group("sites.id").order('COUNT(favorite_entries.id) DESC').limit(3)
             end
         end
         if params[:friends]
