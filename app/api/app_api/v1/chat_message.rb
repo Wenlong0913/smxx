246b7b05ceacb6fed42f72c8d7ed6e85b34828c3
cheng <cheng@tanmer.com>
2017-04-14 11:53:31 +0800

    edit

--- app/api/app_api/v1/chat_message.rb
+++ app/api/app_api/v1/chat_message.rb
@@ -16,19 +16,6 @@ module AppAPI::V1
         wrap_collection messages, AppAPI::Entities::ChatMessage
       end
 
-      # desc '聊天回复' do
-      # end
-      # params do
-      #   requires :room_id, type: Integer, desc: '频道ID'
-      #   requires :user_id, type: Integer, desc: '发送用户ID'
-      #   requires :test, type: String, desc: '消息内容'
-      # end
-      # post do
-      #   room = ::Chat::Room.find_by(id: params[:room_id])
-      #   error! '频道不存在' unless room
-      #
-      # end
-
     end # end of resources
   end
 end
