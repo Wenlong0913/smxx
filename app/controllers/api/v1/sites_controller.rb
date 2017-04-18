de3a85627323abc16a897120c0485aaab276e81e
linan <linan.avvo@gmail.com>
2017-04-18 14:44:12 +0800

    fix search site bug

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SitesController < Api::BaseController
   def index
     # authorize Site
     page_size = params[:page_size].present? ? params[:page_size].to_i : 20
-    sites = params['name'].present? ? Site.where("title like :key", {key: ['%',params['name'].upcase, '%'].join}) : Site.all
+    sites = params['name'].present? ? Site.where("title like :key", {key: ['%',params['name'], '%'].join}) : Site.all
     sites = sites.order(created_at: :desc).page(params[:page] || 1).per(page_size)
     render json: {
       sites: site_json(sites),
