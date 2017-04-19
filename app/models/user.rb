6435afb01a97185d20d6d104ee0eb1ff0ae06d11
xiaohui <xiaohui@zhangxh.net>
2017-04-19 11:00:17 +0800

    修复头像url的bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -117,7 +117,7 @@ class User < ApplicationRecord
       else
         headshot
       end
-    URI(Settings.site.host).merge( url || "/assets/no-picture.png")
+    URI(Settings.site.host).merge( url || "/assets/no-picture.png").to_s
   end
 
 end
