bb9a1f3fe876008a9c117dabe2e97ba7f8f24241
xiaohui <xiaohui@zhangxh.net>
2017-04-20 11:38:56 +0800

    move mobile to meikemei-app

--- mobile/src/components/main.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <f7-list>
-    <f7-list-item link="/login/" title="Login"></f7-list-item>
-  </f7-list>
-</template>
--- mobile/src/store/mutation-types.js
+++
@@ -1,1 +0,0 @@
-export const CHANGE_VIEW_TITLE = 'common/change_view_title'
