54497bfe6c3cfc19406326caf6d0d16c0eb5b274
xiaohui <xiaohui@zhangxh.net>
2017-04-24 14:55:24 +0800

    fixing db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,22 @@
   Role.find_or_create_by name: name
 end
 
-_, admin = User::Create.(mobile_phone: '18080810818', nickname: '管理员', password: 'abcd1234')
-puts admin.save!
-raise "创建的第一个用户ID不等于1!!!" unless admin.id == 1
+unless User.find_by_phone_number('18080810818')
+  _, admin = User::Create.(mobile_phone: '18080810818', nickname: '管理员', password: 'abcd1234')
+  admin.save!
+  raise "创建的第一个用户ID不等于1!!!" unless admin.id == 1
+  admin.add_role :admin
+  admin.add_role :super_admin
+end
 
-_, agent = User::Create.(mobile_phone: '15983288999', nickname: '商家', password: 'abcd1234')
-_, user = User::Create.(mobile_phone: '15328077520', nickname: '用户', password: 'abcd1234')
+unless User.find_by_phone_number('15983288999')
+  _, agent = User::Create.(mobile_phone: '15983288999', nickname: '商家', password: 'abcd1234')
+  agent.add_role :agent
+end
 
-admin.add_role :admin
-admin.add_role :super_admin
-agent.add_role :agent
+unless User.find_by_phone_number('15328077520')
+  _, user = User::Create.(mobile_phone: '15328077520', nickname: '用户', password: 'abcd1234')
+end
 
 site = Site.create_with(user: admin, address_line: '成都市成华区二环路东二段龙湖三千城').find_or_create_by!(title: '官网')
 raise "创建的第一个商家ID不等于1!!!" unless site.id == 1
@@ -35,14 +41,19 @@ Keystore.put('cms_template_names', "['default','app-landing-spotlight']")
 
 # init Cms
 # visit: http://localhost:3000/cms_1/
-cms_site = Cms::Site.create!(site_id: site.id, name: '企业官网', domain: 'www', template: 'default', description: '这是用CMS搭建的官网')
+cms_site =
+  Cms::Site.create_with(name: '企业官网', domain: 'www', template: 'default', description: '这是用CMS搭建的官网').
+  find_or_create_by(site_id: site.id)
 # Cms::Site after_create :initialize_channel　已经存在，会自动创建一个首页
 # cms_channel = Cms::Channel.create!(site_id: cms_site.reload.id, title: '首页', description: '这里是首页的栏目描述', short_title: 'index', tmp_index: 'temp_index.html.erb', tmp_detail: 'temp_detail.html.erb')
-cms_channel = Cms::Channel.create!(site_id: cms_site.reload.id, title: '新闻列表', description: '这里是新闻的栏目描述', short_title: 'news', tmp_index: 'temp_news_list.html.erb', tmp_detail: 'temp_detail.html.erb')
+cms_channel =
+  Cms::Channel.create_with(title: '新闻列表', description: '这里是新闻的栏目描述', short_title: 'news', tmp_index: 'temp_news_list.html.erb', tmp_detail: 'temp_detail.html.erb').
+  find_or_create_by(site_id: cms_site.reload.id)
+
 content = ''
 5.times{|i| content += '<p>这是内容部分!</p>'}
 5.times do
-  cms_page = Cms::Page.create!(channel_id: cms_channel.reload.id, title: '这是新闻标题', description: '这里是页面的描述', content: content)
+  cms_page = Cms::Page.find_or_create_by(channel_id: cms_channel.reload.id, title: '这是新闻标题', description: '这里是页面的描述', content: content)
 end
 
 names = %w(维修
@@ -72,12 +83,16 @@ end
 puts "创建商家"
 vendor_names = ["自购", "舞东风", "联合100", "友达", "顺丰", "壹佰", "盛世百龙", "义力"]
 vendor_names.each do |vendor_name|
-  _, vendor_user = Vendor::Create.(name: vendor_name, contact_name: vendor_name + '联系人', phone_number: '152133643' + (10..99).to_a.sample(1).join)
+  unless Vendor.exists?(name: vendor_name)
+    _, vendor_user = Vendor::Create.(name: vendor_name, contact_name: vendor_name + '联系人', phone_number: '152133643' + (10..99).to_a.sample(1).join)
+  end
 end
 
 # 创建仓库
 puts "创建初始仓库"
-MaterialWarehouse::Create.(site_id: Site.first.id, name: '初始仓库')
+unless MaterialWarehouse.exists?(site_id: Site.first.id)
+  MaterialWarehouse::Create.(site_id: Site.first.id, name: '初始仓库')
+end
 
 # 德格物料分类 & 物料
 puts "创建物料分类 & 物料"
@@ -87,32 +102,40 @@ material_catalogs = {
   "平开门": [{'五金': []}, {'吸塑': []}]
 }
 material_catalogs.each_pair do |material_catalog, sub_material_catalos|
-  _, material_catalog = MaterialCatalog::Create.(name: material_catalog)
+  material_catalog = MaterialCatalog.find_by(name: material_catalog)
+  unless material_catalog
+    _, material_catalog = MaterialCatalog::Create.(name: material_catalog)
+  end
   sub_material_catalos.each do |sub_material|
     sub_material.each_pair do |next_catalog, materials|
-      _, next_catalog = MaterialCatalog::Create.(name: next_catalog, parent: material_catalog)
+      next_catalog = MaterialCatalog.find_by(name: next_catalog, parent: material_catalog)
+      unless next_catalog
+        _, next_catalog = MaterialCatalog::Create.(name: next_catalog, parent: material_catalog)
+      end
       5.times do | index |
-        _, material = Material::Create.(name: next_catalog.name + (index+1).to_s, catalog: next_catalog, site_id: Site.first.id)
+        unless Material.exists?(name: next_catalog.name + (index+1).to_s, catalog: next_catalog, site_id: Site.first.id)
+          _, material = Material::Create.(name: next_catalog.name + (index+1).to_s, catalog: next_catalog, site_id: Site.first.id)
+        end
       end
     end
   end
 end
 
 # 客户
-MemberCatalog.create!(key: '客户级别', value: '{A类,B类,C类}')
-MemberCatalog.create!(key: '会员关系', value: '{非会员,普通会员,银卡会员,金卡会员,至尊会员}')
-MemberCatalog.create!(key: '客户性别', value: '{男,女}')
-MemberCatalog.create!(key: '客户年龄', value: '{15-20,20-30,30-40,40-50,50-60}')
-MemberCatalog.create!(key: '购买能力', value: '{一般,高,很高}')
-MemberCatalog.create!(key: '客户活跃频次', value: '{很少,一般,高,很高}')
+MemberCatalog.find_or_create_by(key: '客户级别', value: '{A类,B类,C类}')
+MemberCatalog.find_or_create_by(key: '会员关系', value: '{非会员,普通会员,银卡会员,金卡会员,至尊会员}')
+MemberCatalog.find_or_create_by(key: '客户性别', value: '{男,女}')
+MemberCatalog.find_or_create_by(key: '客户年龄', value: '{15-20,20-30,30-40,40-50,50-60}')
+MemberCatalog.find_or_create_by(key: '购买能力', value: '{一般,高,很高}')
+MemberCatalog.find_or_create_by(key: '客户活跃频次', value: '{很少,一般,高,很高}')
 
 # 营销页
-MarketCatalog.create!(name: '博客')
-MarketCatalog.create!(name: '活动')
-MarketCatalog.create!(name: '招商')
-MarketCatalog.create!(name: '会员拉新')
+MarketCatalog.find_or_create_by(name: '博客')
+MarketCatalog.find_or_create_by(name: '活动')
+MarketCatalog.find_or_create_by(name: '招商')
+MarketCatalog.find_or_create_by(name: '会员拉新')
 
-MarketTemplate.create!(
+MarketTemplate.find_or_create_by(
   catalog_id: 1,
   base_path: 'templetes/market/default',
   name: 'default',
@@ -174,9 +197,9 @@ if Settings.project.imolin?
     c.is_published = !site_worksheet.row(index)[head.find_index('is_closed')]
     c.province = site_worksheet.row(index)[head.find_index('province')]
     c.city = site_worksheet.row(index)[head.find_index('city')]
-    c.area = site_worksheet.row(index)[head.find_index('area')]
+    # c.area = site_worksheet.row(index)[head.find_index('area')]
     c.catalog = SiteCatalog.find_or_create_by_path([{name: site_worksheet.row(index)[head.find_index('big_cate')]},{name: site_worksheet.row(index)[head.find_index('small_cate')]}])
-    c.address_line = c.province + c.city + c.area + site_worksheet.row(index)[head.find_index('address')] + ' ' + c.title
+    c.address_line = c.province + c.city + site_worksheet.row(index)[head.find_index('address')] + ' ' + c.title
     c.business_area = site_worksheet.row(index)[head.find_index('business_area')]
     c.phone = site_worksheet.row(index)[head.find_index('phone')]
     c.business_hours = site_worksheet.row(index)[head.find_index('hours')]
