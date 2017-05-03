0bb18cdce6dab05c2f6989450d2dc6b8eac7cab5
yegang <yegang@tanmer.com>
2017-05-03 16:48:50 +0800

    api返回商品的详情字段

--- app/api/app_api/entities/product.rb
+++ app/api/app_api/entities/product.rb
@@ -19,7 +19,8 @@ module AppAPI
         expose :unit, documentation: { desc: '库存单位' }
         expose :price, documentation: { desc: '产品原价', type: Float }
         expose :discount, documentation: { desc: '产品折后价', type: Float }
-        expose :description, documentation: { desc: '产品的详细信息描述' }
+        expose :description, documentation: { desc: '产品简要描述' }
+        expose :content, documentation: {desc: Settings.project.meikemei? ? '项目介绍' : '产品介绍'}
         expose :is_fee, documentation: { desc: '是否有物流费用', type: Grape::API::Boolean }
         expose :shopping_fee, documentation: { desc: '物流费用', type: Float }
         expose :weight, documentation: { desc: '产品重量', type: Float}
@@ -29,6 +30,7 @@ module AppAPI
 
       def product_special_attributes
         psa = {}
+        return psa if object.additional_attribute_keys.blank?
         object.additional_attribute_keys.each_pair do |k, v|
           psa[v] = object.additional_attribute_values[k]
         end
