d548ef0f2c045a2447311ac15ce90fb0fde33901
yegang <yegang@tanmer.com>
2017-05-03 21:15:31 +0800

    评论的点赞数和评论数

--- app/api/app_api/entities/comment.rb
+++ app/api/app_api/entities/comment.rb
@@ -9,6 +9,12 @@ module AppAPI
       expose :content, documentation: { desc: '内容' }
       expose :user, using: AppAPI::Entities::UserSimple
       expose :parent_id, documentation: { desc: '父级评论ID' }
+      expose :comments_count, documentation: { desc: '本条评论的回复数' }
+      expose :likes_count, documentation: { desc: '本条评论的点赞数' }
+
+      def comments_count
+        object.children.size
+      end
     end
   end
 end
