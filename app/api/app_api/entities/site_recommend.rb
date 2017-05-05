f3f5ef3b4888bacd883710b48aa1725f399a4862
yegang <yegang@tanmer.com>
2017-05-05 17:58:27 +0800

    店长推荐返回产品名称

--- app/api/app_api/entities/site_recommend.rb
+++ app/api/app_api/entities/site_recommend.rb
@@ -2,6 +2,7 @@ module AppAPI
   module Entities
     class SiteRecommend < Base
       expose_id
+      expose :name, documentation: { desc: "产品名称" }
       expose :image_url, documentation: { desc: '产品图片' }
       expose :redirect_web_url, documentation: { desc: '跳转到web的链接' }
       expose :redirect_app_url, documentation: { desc: '跳转到app的链接' }
