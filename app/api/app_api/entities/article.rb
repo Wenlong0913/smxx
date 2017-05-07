d587ba4cfdca50a85a626acf542b9097abd3d9f7
yegang <yegang@tanmer.com>
2017-05-07 13:16:01 +0800

    文章列表返回内容和产品信息

--- app/api/app_api/entities/article.rb
+++ app/api/app_api/entities/article.rb
@@ -1,9 +1,14 @@
 module AppAPI
   module Entities
     class Article < ArticleSimple
-      expose :description, documentation: { desc: '文章内容' }
-      expose :products, using: AppAPI::Entities::ProductSimple, documentation: { is_array: true }
+      expose :description, documentation: { desc: '文章内容' }, if: ->(article, options) { options.fetch(:includes, []).map(&:to_s).include?('description') }
+      expose :products_simple, as: :products, if: ->(article, options) { options[:type] == :list }
+      expose :products, using: AppAPI::Entities::ProductSimple, documentation: { is_array: true }, if: ->(article, options) { options[:type] != :list }
       expose_updated_at
+
+      def products_simple
+        object.products.as_json(only: [:name], methods: :price, include: { image_items: { only: [], methods: [:image_url]} })
+      end
     end
   end
 end
