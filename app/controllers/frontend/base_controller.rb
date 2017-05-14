21d9573b2e1a2c82545947c06e2d1444bfaea870
宋学江 <song@tanmer.com>
2017-05-14 23:26:37 +0800

    updage frontend

--- app/controllers/frontend/base_controller.rb
+++ app/controllers/frontend/base_controller.rb
@@ -1,4 +1,12 @@
 class Frontend::BaseController < ApplicationController
   layout 'layouts/frontend'
+  helper Cms::ApplicationHelper
+  include Cms::ApplicationHelper
+  before_action :load_cms_site
 
+  private
+  def load_cms_site
+    @cms_site = Cms::Site.where("domain = ? OR root_domain = ?", request.subdomain, request.domain).first
+    @cms_site ||= Cms::Site.first
+  end
 end
--- app/controllers/frontend/home_controller.rb
+++ app/controllers/frontend/home_controller.rb
@@ -1,4 +1,6 @@
-class Frontend::HomeController < ApplicationController
+class Frontend::HomeController < Frontend::BaseController
   def index
+    @news_channel = @cms_site.channels.find_by(short_title: 'news')
+    @product = Product.first
   end
 end
