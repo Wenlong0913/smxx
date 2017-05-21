249a220751284092bb3fc66287f3c11b19eab9bd
jianyi <jianyi@tanmer.com>
2017-05-22 01:02:12 +0800

    fix tools/upload_icon_to_site_catalog.rb

--- tools/upload_icon_to_site_catalog.rb
+++ tools/upload_icon_to_site_catalog.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../config/environment', __FILE__)
 class UploadIconToSiteCatalog
   def run
     SiteCatalog.all.each do |site_catalog|
-      site_catalog.icon_url = URI(Settings.site.host).merge(site_catalog.name + '.png').to_s
+      site_catalog.icon_url = "#{Settings.site.host}/icons/#{URI::escape(site_catalog.name)}.png"
       site_catalog.save!
     end
   end
