4ec22c3c2dee0a2719d0cb624469735a24a51b56
hao <hao@tanmer.com>
2017-05-26 18:46:47 +0800

    update

--- app/controllers/frontend/share_controller.rb
+++ app/controllers/frontend/share_controller.rb
@@ -1,6 +1,15 @@
 class Frontend::ShareController < Frontend::BaseController
   def index
-    @site = Site.find_by(id: params[:site_id])
-    @site_user = @site.user
+    if params[:site_id].present?
+      @site = Site.where(id: params[:site_id]).first
+      if @site.blank?
+        @share_message = @cms_site
+        redirect_to frontend_path
+      else
+        @share_message = @site.user
+      end
+    else
+      @share_message = @cms_site
+    end
   end
 end
