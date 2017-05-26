3e5e1809e4bcc1ee50d94b9b14028099d90461c5
linan <linan.avvo@gmail.com>
2017-05-26 18:38:48 +0800

    修改价格显示

--- app/api/app_api/entities/order_simple.rb
+++ app/api/app_api/entities/order_simple.rb
@@ -7,8 +7,13 @@ module AppAPI
       expose_created_at
       expose_updated_at
       expose :code, documentation: { desc: '订单号' }
-      expose :price, documentation: { desc: '价格', type: Float }
+      # expose :price, documentation: { desc: '价格', type: Float }
       expose :status, documentation: { desc: '状态' }
+      expose :display_price, as: :price, documentation: { desc: '产品价格', type: Float}
+
+      def display_price
+        object.price.to_f/100
+      end
     end
   end
 end
--- app/api/app_api/entities/shopping_cart.rb
+++ app/api/app_api/entities/shopping_cart.rb
@@ -5,9 +5,14 @@ module AppAPI
       # public attributes
       expose_id
       expose :amount, documentation: { desc: '产品数量', type: Integer }
-      expose :price, documentation: { desc: '产品价格', type: Float }
+      # expose :price, documentation: { desc: '产品价格', type: Float }
       expose :product, using: AppAPI::Entities::Product, if: lambda { |instance, options| (options[:includes] || []).include?(:product) }
       expose :site_name, if: lambda { |instance, options| (options[:includes] || []).include?(:site) }
+      expose :display_price, documentation: { desc: '产品价格', type: Float}, as: :price
+
+      def display_price
+        object.price.to_f/100
+      end
 
       def site_name
         object.product.site.title
