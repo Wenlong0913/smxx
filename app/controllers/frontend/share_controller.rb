33dc7e3b300380da1005a29f2484e56a24eb8bef
hao <hao@tanmer.com>
2017-05-27 21:01:52 +0800

    update share controller

--- app/controllers/frontend/share_controller.rb
+++ app/controllers/frontend/share_controller.rb
@@ -1,13 +1,20 @@
 class Frontend::ShareController < Frontend::BaseController
   def index
-    if params[:site_id].present?
-      @site = Site.where(id: params[:site_id]).first
-      if @site.blank?
-        @share_message = @cms_site
-        redirect_to frontend_path
-      else
-        @share_message = @site.user
+
+    if params[:class].present? && params[:id].present?
+
+      if params[:class] == 'User'
+          @share_message = User.where(id: params[:id]).first
       end
+
+      if params[:class] == 'Community'
+        @share_message = Community.where(id: params[:id]).first
+      end
+
+      if params[:class] == 'Product'
+        @share_message = Product.where(id: params[:id]).first
+      end
+
     else
       @share_message = @cms_site
     end
