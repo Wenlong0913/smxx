1a311b5ef8d04571628a9cde86376cfc5dff7606
宋学江 <song@tanmer.com>
2017-05-31 20:45:11 +0800

    upfe

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -14,7 +14,8 @@ class CmsController < ApplicationController
   # 1. a URL query must has channel except root
   # 2. If has page, the channel is page.channel, not care the params
   def index
-    if request.original_url =~ /http:\/\/www.imolin.cn/share/i
+    puts request.original_url
+    if request.original_url =~ /http:\/\/www.imolin.cn\/share/i
       return redirect_to "http://www.imolin.cn/frontend/share/sites/378764"
     end
     #page first, then channel ?
