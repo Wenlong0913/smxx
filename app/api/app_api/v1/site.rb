ca639881836253215ee3edad7da91b4b1f7a66d3
cheng <cheng@tanmer.com>
2017-06-06 10:24:04 +0800

    添加小区附近商家api

--- app/api/app_api/v1/site.rb
+++ app/api/app_api/v1/site.rb
@@ -54,6 +54,21 @@ module AppAPI::V1
         wrap_collection sites, AppAPI::Entities::SiteSimple, includes: [:distance]
       end
 
+      desc "获取附近#{::Site.model_name.human}列表" do
+        success AppAPI::Entities::SiteSimple.collection
+      end
+      params do
+        use :pagination
+      end
+      get '/near_by'do
+        authenticate!
+        sites = ::Site.all
+        community = ::Community.find_by(id: current_user.current_community.id)
+        sites = sites.near_by(lat: community.address_lat, lng: community.address_lng, distance: 5000)
+        sites = paginate_collection(sort_collection(sites), params)
+        wrap_collection sites, AppAPI::Entities::SiteSimple, includes: [:distance]
+      end
+
       desc '店铺站长推荐'
       params do
         requires :id, type: Integer, desc: "#{::Site.model_name.human}ID"
