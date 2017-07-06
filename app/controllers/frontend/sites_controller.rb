92586a99d2010e588a20632df4b2330246708328
wen <gang@tanmer.com>
2017-07-06 14:56:43 +0800

    更新site评论

--- app/controllers/frontend/sites_controller.rb
+++ app/controllers/frontend/sites_controller.rb
@@ -2,5 +2,6 @@ class Frontend::SitesController < Frontend::BaseController
 	acts_as_commentable resource: Site
   def show
     @site = Site.find(params[:id])
+    @comment_path = comments_frontend_site_path
   end
 end
