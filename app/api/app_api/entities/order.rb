b175626d7f1c8b1536af0448dd26e8086fbc809a
linan <linan.avvo@gmail.com>
2017-07-14 16:33:13 +0800

    order api add

--- app/api/app_api/entities/order.rb
+++ app/api/app_api/entities/order.rb
@@ -4,6 +4,14 @@ module AppAPI
       expose :description, documentation: { desc: '订单描述备注信息'}
       expose :order_products, using: AppAPI::Entities::OrderProduct, documentation: { is_array: true }
       expose :order_deliveries, using: AppAPI::Entities::OrderDelivery, documentation: { desc: '订单物流信息', is_array: true }
+      if Settings.project.meikemei?
+        expose :service_time
+        expose :staff
+
+        def staff
+          ::Staff.find_by(id: object.staff_id).as_json(only: [:title], methods: [:headshot])
+        end
+      end
     end
   end
 end
