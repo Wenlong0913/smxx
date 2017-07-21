527140171f5765c1a6c7476f3234849837c59750
YeGang <yegang@tanmer.com>
2017-07-21 16:57:39 +0800

    app_setting转数据库存储

--- app/api/app_api/v1/app_settings.rb
+++ app/api/app_api/v1/app_settings.rb
@@ -5,7 +5,16 @@ module AppAPI::V1
 
       desc 'App配置信息'
       get do
-        Rails.application.config_for(:app_settings)
+        info = AppSetting.where(key_word: 'APP-Setting').first
+        app_data = {
+          site_share_url_pattern: info.site_share_url_pattern,
+          product_share_url_pattern: info.product_share_url_pattern,
+          system_rooms: begin eval(info.system_rooms) rescue nil end,
+          service_banners: begin eval(info.service_banners) rescue nil end,
+          main_banners: begin eval(info.service_banners) rescue nil end,
+          app_version_message: begin eval(info.app_version_message) rescue nil end
+        }
+        present app_data
       end
 
     end # end of resources
---
+++ db/migrate/20170720091631_create_app_settings.rb
@@ -0,0 +1,11 @@
+class CreateAppSettings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :app_settings do |t|
+      t.string :name
+      t.string :key_word
+      t.json :app_data
+
+      t.timestamps
+    end
+  end
+end
