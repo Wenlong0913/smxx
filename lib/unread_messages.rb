e34dc492f60b0fc2e7f53250132f1df89d450d3e
linan <linan.avvo@gmail.com>
2017-07-25 15:44:49 +0800

    优化代码

--- lib/unread_messages.rb
+++ lib/unread_messages.rb
@@ -1,43 +1,26 @@
 class UnreadMessages
   def initialize(room_id, user_id)
-    @room = Chat::Room.find_by(id: room_id)  
+    @room = Chat::Room.find_by(id: room_id)
+    @room_id = "room-#{@room.id.to_s}"  
     @current_user = User.find_by(id: user_id)
     @redis = Redis.current
   end
 
   def user_add_room
-    room_id = 'room-' + @room.id.to_s
-    room_users = @redis.get(room_id)
-    if room_users
-      room_users_json = JSON.parse(room_users)
-      room_users_json["online_users"].push(@current_user.id)
-      @redis.set(room_id, room_users_json.to_json)
-    else
-      room_users_json = {}
-      room_users_json["online_users"] = [@current_user.id]
-      @redis.set(room_id, room_users_json.to_json)
-    end
+    @redis.sadd(@room_id, @current_user.id)
     clear_user_unread_message(@current_user.id)
   end
 
   def user_leave_room
-    room_id = 'room-' + @room.id.to_s
-    room_users = @redis.get(room_id)
-    if room_users
-      room_users_json = JSON.parse(room_users)
-      room_users_json["online_users"].delete(@current_user.id)
-      @redis.set(room_id, room_users_json.to_json)
-    end
+    @redis.srem(@room_id, @current_user.id)
   end
 
   def add_unread_message
     all_users =  @room.owner_type == 'Community' ? @room.owner.users.map(&:id) : []
-    room_id = 'room-' + @room.id.to_s
-    current_users = @redis.get(room_id)
+    current_users = @redis.smembers(@room_id)
     offline_users = []
     if current_users
-      current_users_json = JSON.parse(current_users)
-      offline_users = all_users.uniq - current_users_json["online_users"].uniq
+      offline_users = all_users.uniq.map(&:to_s) - current_users
     else
       offline_users = all_users
     end
@@ -47,36 +30,25 @@ class UnreadMessages
   end
 
   def add_user_unread_message(user_id)
-    user_with_id = 'user-' + user_id.to_s
-    room_with_id = 'room-' + @room.id.to_s
-    user_unread_messages = @redis.get(user_with_id)
-    user_unread_messages_json = user_unread_messages ? JSON.parse(user_unread_messages) : {}
-    user_unread_messages_json[room_with_id] = user_unread_messages_json[room_with_id] ? (user_unread_messages_json[room_with_id] + 1) : 1
-    @redis.set(user_with_id, user_unread_messages_json.to_json)
+    user_room_unread_message_key = "user-#{user_id}-room-#{@room.id}"
+    user_room_unread_message_count = @redis.get(user_room_unread_message_key) || 0
+    @redis.set(user_room_unread_message_key, user_room_unread_message_count.to_i + 1)
     push_unread_message(user_id)
   end
 
   def push_unread_message(user_id)
     user = User.find(user_id)
-    user_id = 'user-' + user_id.to_s
-    user_unread_messages = @redis.get(user_id)
-    user_unread_messages_json = user_unread_messages ? JSON.parse(user_unread_messages) : {}
+    rooms = @redis.keys("user-#{user_id}-room-*")
     push_messages = []
-    user_unread_messages_json.each_pair do |k, v|
-      push_messages.push({id: k.split('room-')[1], count: v})
+    rooms.each do |key|
+      push_messages.push({id: key.split('room-')[1], count: @redis.get(key)})
     end
     UserChannel.broadcast_to user, message: "#{push_messages.to_json}", type: 'room'
   end
 
   def clear_user_unread_message(user_id)
-    user_with_id = 'user-' + user_id.to_s
-    room_with_id = 'room-' + @room.id.to_s
-    user_unread_messages = @redis.get(user_with_id)
-    if user_unread_messages
-      user_unread_messages_json = JSON.parse(user_unread_messages)
-      user_unread_messages_json[room_with_id] = 0
-      @redis.set(user_with_id, user_unread_messages_json.to_json)
-    end
+    user_room_unread_message_key = "user-#{user_id}-room-#{@room.id}"
+    @redis.set(user_room_unread_message_key, 0)
     push_unread_message(user_id)
   end
 end
