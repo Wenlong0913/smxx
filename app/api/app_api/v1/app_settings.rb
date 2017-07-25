086f1a4760e1b8e228e73b29abc264927552d003
YeGang <yegang@tanmer.com>
2017-07-25 18:43:50 +0800

    添加app_setting初始数据和修改api返回的数据

--- app/api/app_api/v1/app_settings.rb
+++ app/api/app_api/v1/app_settings.rb
@@ -9,10 +9,10 @@ module AppAPI::V1
         app_data = {
           site_share_url_pattern: current_setting.site_share_url_pattern,
           product_share_url_pattern: current_setting.product_share_url_pattern,
-          system_rooms: begin YAML.load(current_setting.system_rooms) rescue nil end,
-          service_banners: begin YAML.load(current_setting.service_banners) rescue nil end,
-          main_banners: begin YAML.load(current_setting.service_banners) rescue nil end,
-          app_version_message: begin YAML.load(current_setting.app_version_message) rescue nil end
+          system_rooms: begin YAML.load(current_setting.system_rooms) rescue [] end,
+          service_banners: begin YAML.load(current_setting.service_banners) rescue [] end,
+          main_banners: begin YAML.load(current_setting.service_banners) rescue [] end,
+          app_version_message: begin YAML.load(current_setting.app_version_message) rescue {} end
         }
         present app_data
       end
