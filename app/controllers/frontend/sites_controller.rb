8dcd33bb1bc0dd780284212297705630b55ee29b
wen <gang@tanmer.com>
2017-08-08 17:58:04 +0800

    update

--- app/controllers/frontend/sites_controller.rb
+++ app/controllers/frontend/sites_controller.rb
@@ -1,8 +1,10 @@
 class Frontend::SitesController < Frontend::BaseController
 	acts_as_commentable resource: Site
   impressionist :actions=>[:show]
+
   def show
     @site = Site.find(params[:id])
-    @comment_path = comments_frontend_site_path
+    @comment_path = comments_frontend_site_path(@site)
   end
+  
 end
