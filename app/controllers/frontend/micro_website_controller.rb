6e68a7c501241dbecc6dec3e8cc31f178b120caa
hao <hao@tanmer.com>
2017-08-25 22:56:50 +0800

    优化代码块

--- app/controllers/frontend/micro_website_controller.rb
+++ app/controllers/frontend/micro_website_controller.rb
@@ -14,22 +14,12 @@ class Frontend::MicroWebsiteController < Frontend::BaseController
   def wechat_products
     if params[:id]
       @product_catalog = ProductCatalog.find(params[:id])
-      @products = @product_catalog.products.page(params[:page])
+      @products = @product_catalog.products
     else
-      @products = Product.all.page(params[:page])
+      @products = Product.all
     end
-    if params[:type] == 'pending'
-      @products = Product.where("features ->> 'status' = ?", 'pending').page(params[:page])
-    end
-    if params[:type] == 'open'
-      @products = Product.where("features ->> 'status' = ?", 'open').page(params[:page])
-    end
-    if params[:type] == 'completed'
-      @products = Product.where("features ->> 'status' = ?", 'completed').page(params[:page])
-    end
-    if params[:type] == 'closed'
-      @products = Product.where("features ->> 'status' = ?", 'closed').page(params[:page])
-    end
+    @products = @products.where("features ->> 'status' = ?", params[:type]) if %w(pending open completed closed).include?(params[:type])
+    @products = @products.page(params[:page])
   end
 
   def wechat_product
