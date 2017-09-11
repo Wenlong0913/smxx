35f5e751c9b02d913a91bdf98e532681d001c497
linan <linan.avvo@gmail.com>
2017-09-11 15:11:20 +0800

    验票

--- app/controllers/agent/products_controller.rb
+++ app/controllers/agent/products_controller.rb
@@ -85,6 +85,19 @@ class Agent::ProductsController < Agent::BaseController
     respond_to do |format|
       format.html
       format.json { render json: @product }
+      format.png do
+        qrcode = RQRCode::QRCode.new(wechat_orders_micro_website_url(product_id: @product.id))
+        send_data qrcode.as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 240,
+          border_modules: 4,
+          module_px_size: 6,
+          file: nil
+        )
+      end
     end
   end
 
