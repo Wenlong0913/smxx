3533650bc3ed07a0f15c8e3597e4cbf154118376
linan <linan.avvo@gmail.com>
2017-09-15 13:59:49 +0800

    add

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -30,6 +30,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
       sign_up_with_mobile(params[:user][:mobile], params[:user][:code])
     elsif params[:user][:email].present?
       sign_up_with_email(params[:user][:email], params[:user][:password], params[:user][:password_confirmation])
+    elsif params[:user][:weixin_code].present?
+      conn = Faraday.new(:url => Settings.weixin_login.host)
+      response = conn.get('/wx/mp_auth/%s/fetch_uid/%s' % [Settings.weixin_login.appid, params[:user][:weixin_code]])
+      data = JSON.parse(response.body)
+      if data['uid']
+        sign_up_with_weixin(data['uid'])
+      else
+        render json: { error: '微信注册失败,请重试' }
+      end
     end
   end
 
@@ -64,6 +73,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
       sign_in user, :event => :authentication
       render json: {}
     else
+      render json: {error: user.errors.full_messages.join(',')}
+    end
+  end
+
+  def sign_up_with_weixin(uid)
+    user = User.new
+    weixin = User::Weixin.find_or_initialize_by(uid: uid)
+    if weixin.new_record?
+      user.password ||= Devise.friendly_token[0,20]
+      user.weixin = weixin
+      user.nickname = weixin.name
+      if user.save
+        sign_in user, :event => :authentication
+        render json: {} 
+      else
+        render json: {error: user.errors.full_messages.join(',')}
+      end
+    else
+      user.errors.add :weixin, '已经存在'
       render json: {error: user.errors.full_messages.join(',')}
     end
   end
