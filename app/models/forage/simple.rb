e28f6bc0e5040b168cf90b6a913ab533dad245c4
YeGang <yegang@tanmer.com>
2017-10-16 21:00:39 +0800

    optimize

--- app/models/forage/simple.rb
+++ app/models/forage/simple.rb
@@ -1,6 +1,8 @@
 class Forage::Simple < ApplicationRecord
   audited
   belongs_to :run_key, class_name: '::Forage::RunKey'
+
+  store_accessor :features, :original_catalog, :district_from
   has_many :details, dependent: :destroy
   validates_uniqueness_of :url
 end
