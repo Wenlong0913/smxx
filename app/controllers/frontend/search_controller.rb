98de8a1b476723c87c4f017579138fa112e647c3
wenlingang <gang@tanmer.com>
2017-11-24 14:31:25 +0800

    修改搜索判断和显示

--- app/controllers/frontend/search_controller.rb
+++ app/controllers/frontend/search_controller.rb
@@ -1,19 +1,18 @@
 class Frontend::SearchController < Frontend::BaseController
   def search_result
-    if params[:type] == nil || params[:type] == 'cms_pages'
+    if params[:type] == nil || params[:type] == 'products'
+      @filter_colums = %w(name)
+      @search_results = build_query_filter(Product.where("features ->>  'is_shelves' = ?", '1').all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
+    end
+
+    if params[:type] == 'cms_pages'
       @filter_colums = %w(title description)
-      @search_results = build_query_filter(Cms::Page.all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
+      @search_results = build_query_filter(@cms_site.pages.all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
     end
 
     if params[:type] == 'sites'
       @filter_colums = %w(title)
-      @search_results = build_query_filter(Site.all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
+      @search_results = build_query_filter(Site.where("features ->> 'is_published' = ?", '1').all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
     end
-
-    if params[:type] == 'products'
-      @filter_colums = %w(name)
-      @search_results = build_query_filter(Product.all, only: @filter_colums).order(updated_at: :desc).page(params[:page])
-    end
-
   end
 end
