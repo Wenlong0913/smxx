119917b27965aed43d1e29c917b1983050815c60
xiaohui <xiaohui@zhangxh.net>
2017-12-18 22:42:25 +0800

    修复actioncable卡死的问题

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -8,12 +8,14 @@ module ApplicationCable
 
     private
     def find_user
-      if request.params[:credential].present?
-        api_token = ApiToken.find_by(token: request.params[:credential])
-        api_token && api_token.user
-      else
-        request::env['warden'].authenticate(scope: :user)
-      end
+      user =
+        if request.params[:credential].present?
+          api_token = ApiToken.find_by(token: request.params[:credential])
+          api_token && api_token.user
+        else
+          request::env['warden'].authenticate(scope: :user)
+        end
+      user || reject_unauthorized_connection
     end
   end
 end
